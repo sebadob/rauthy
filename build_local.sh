1a7e79dc96d27d8d180d1e4394644c8851cbdf70
sd <sd@netit-services.com>
2023-07-05 08:07:16 +0200

    default to scratch image

--- build_local.sh
+++ build_local.sh
@@ -8,9 +8,15 @@ cp target/x86_64-unknown-linux-musl/release/rauthy out/
 # registry.netitservices.com is an internal development registry
 docker build --no-cache -t registry.netitservices.com/sd/rauthy:$TAG .
 docker push registry.netitservices.com/sd/rauthy:$TAG
+docker build --no-cache -f Dockerfile.debug -t registry.netitservices.com/sd/rauthy:$TAG-debug .
+docker push registry.netitservices.com/sd/rauthy:$TAG-debug
 
+# tagged public release
 docker tag registry.netitservices.com/sd/rauthy:$TAG sdobedev/rauthy:$TAG
 docker push sdobedev/rauthy:$TAG
+docker tag registry.netitservices.com/sd/rauthy:$TAG-debug sdobedev/rauthy:$TAG-debug
+docker push sdobedev/rauthy:$TAG-debug
 
+# latest public release
 docker tag registry.netitservices.com/sd/rauthy:$TAG sdobedev/rauthy:latest
 docker push sdobedev/rauthy:latest
