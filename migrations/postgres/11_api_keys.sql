3588e4a68ef4df5a72b638673ab37fdce6c4a88d
sebadob <sebastiandobe@mailbox.org>
2023-10-18 14:31:07 +0200

    add 'api_keys' table

---
+++ migrations/postgres/11_api_keys.sql
@@ -0,0 +1,18 @@
+create table api_keys
+(
+    name             varchar           not null
+        constraint api_keys_pk
+            primary key,
+    secret           varchar           not null,
+    is_hash_uptodate bool default true not null,
+    created          bigint            not null,
+    expires          bigint,
+    enc_key_id       varchar           not null,
+    access           bytea  not null
+);
+
+create index api_keys_enc_key_id_index
+    on api_keys (enc_key_id);
+
+create index api_keys_expires_index
+    on api_keys (expires);
