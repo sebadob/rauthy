3fd5976673915fdddf4e35202b3671d7575df564
sebadob <sebastiandobe@mailbox.org>
2023-10-18 13:12:40 +0200

    fix timestamp in 'events' migrations

--- migrations/postgres/10_events.sql
+++ migrations/postgres/10_events.sql
@@ -1,9 +1,9 @@
 create table events
 (
-    id        varchar     not null,
-    timestamp timestamptz not null,
-    typ       varchar     not null,
-    level     smallint    not null,
+    id        varchar  not null,
+    timestamp bigint   not null,
+    typ       varchar  not null,
+    level     smallint not null,
     ip        varchar,
     constraint events_pk
         primary key (id, timestamp)
