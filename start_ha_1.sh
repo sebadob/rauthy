b1fa362e03a2a332fee15d5e1deeb35231ac725f
sebadob <sebastiandobe@mailbox.org>
2023-10-26 12:23:16 +0200

    update start ha scripts for local testing

--- start_ha_1.sh
+++ start_ha_1.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-HA_MODE='true' HA_HOSTS="http://$(hostname):8001,http://127.0.0.1:8002,http://127.0.0.1:8003" cargo run $1
-
+DATABASE_URL="postgresql://rauthy:123SuperSafe@localhost:5432/rauthy" \
+HA_MODE=true \
+HA_HOSTS="http://127.0.0.1:8001,http://127.0.0.1:8002,http://127.0.0.1:8003" \
+HOSTNAME_OVERWRITE="127.0.0.1:8001" \
+cargo run $1
--- start_ha_2.sh
+++ start_ha_2.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-HA_MODE=true HA_HOSTS="http://127.0.0.1:8001,http://$(hostname):8002,http://127.0.0.1:8003" LISTEN_ADDRESS=0.0.0.0:9090 HOST=localhost:9090 cargo run
-
+DATABASE_URL="postgresql://rauthy:123SuperSafe@localhost:5432/rauthy" \
+HA_MODE=true \
+HA_HOSTS="http://127.0.0.1:8001,http://127.0.0.1:8002,http://127.0.0.1:8003" \
+HOSTNAME_OVERWRITE="127.0.0.1:8002" \
+LISTEN_ADDRESS=0.0.0.0 \
+LISTEN_PORT_HTTP=8090 \
+METRICS_PORT=9091 \
+PUB_URL=localhost:8090 \
+cargo run
