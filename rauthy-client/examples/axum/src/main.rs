4fa530258911115df9d47fcf7684a1287ad5a43e
sebadob <sebastiandobe@mailbox.org>
2023-11-23 11:09:57 +0100

    cleanup axum example and add more comments

--- rauthy-client/examples/axum/src/main.rs
+++ rauthy-client/examples/axum/src/main.rs
@@ -6,6 +6,7 @@ use rauthy_client::provider::OidcProvider;
 use std::collections::HashSet;
 use std::net::SocketAddr;
 use std::sync::Arc;
+use rauthy_client::{DangerAcceptInvalidCerts, RauthyHttpsOnly};
 use tracing::{info, Level};
 
 mod config;
@@ -28,31 +29,44 @@ async fn main() -> anyhow::Result<()> {
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
     // we need to start the backend for the rauthy-client
-    rauthy_client::init(None, false, true).await?;
+    rauthy_client::init(None, RauthyHttpsOnly::No, DangerAcceptInvalidCerts::Yes).await?;
 
     let config = RauthyConfig {
+        // If this is Some(_), the principal will have a .is_admin field being set correctly, if
+        // this claim matches.
         admin_claim: Some(JwtClaim {
             typ: JwtClaimTyp::Roles,
             value: "admin".to_string(),
         }),
+        // This claim must always exist for every single user. Without this claim, a user would
+        // not have access to this app. This is used, because usually you never want to just have
+        // all your OIDC users to have access to a certain application.
         user_claim: JwtClaim {
             typ: JwtClaimTyp::Groups,
             value: "user".to_string(),
         },
+        // In almost all cases, this should just match the `client_id`
         allowed_audiences: HashSet::from(["dev-test".to_string()]),
         client_id: "dev-test".to_string(),
+        // If set to 'false', tokens with a non-verified email address will be rejected.
         email_verified: true,
-        iss: "https://iam.rauthy.local/auth/v1".to_string(),
-        // iss: "https://iam.sebastiandobe.de/auth/v1".to_string(),
+        // The issuer URL from your Rauthy deployment
+        iss: "https://rauthy.local/auth/v1".to_string(),
+        // The scopes you want to request. The only mandatory which always needs to exist is
+        // `openid`, the rest is optional and depending on your needs.
         scope: vec![
             "openid".to_string(),
             "email".to_string(),
             "profile".to_string(),
             "groups".to_string(),
         ],
+        // If set to None, the client will be treated as a public client and not provide any
+        // secret to the /token endpoint after the callback. Set a secret for confidential clients.
         secret: None,
         // secret: Some("secretCopiedFromTheRauthyUiIfIsConfidentialClient".to_string(),),
     };
+    // The redirect_uri here must match the URI of this application, where we accept and handle
+    // the callback after a successful login.
     OidcProvider::setup_from_config(config, "http://localhost:3000/callback").await?;
 
     let config = Config::new().await?;
@@ -62,6 +76,7 @@ async fn main() -> anyhow::Result<()> {
         .route("/auth_check", get(routes::get_auth_check))
         .route("/callback", get(routes::get_callback))
         .route("/protected", get(routes::get_protected))
+        // in production, you should add middlewares here with safe default resposne headers
         .with_state(Arc::new(config));
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
