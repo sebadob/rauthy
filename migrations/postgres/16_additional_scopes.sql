9b661bd3a9e1a1713a050e380583ad722b4a647b
sebadob <sebastiandobe@mailbox.org>
2023-12-29 13:56:28 +0100

    add DB migrations

---
+++ migrations/postgres/16_additional_scopes.sql
@@ -0,0 +1,4 @@
+insert into scopes (id, name, attr_include_access, attr_include_id)
+values  ('awyOUa5rjFkowidSw03otjXU', 'address', null, null),
+        ('ImGlzlvfeOrJ9iY394AVeJgJ', 'phone', null, null)
+on conflict do nothing;
