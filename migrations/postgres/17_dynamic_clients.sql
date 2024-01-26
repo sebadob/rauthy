fa96ca1daa2bfd81f46ea3887178c2c2b3a7ae49
sebadob <sebastiandobe@mailbox.org>
2024-01-26 11:38:41 +0100

    update DB migrations

--- migrations/postgres/17_dynamic_clients.sql
+++ migrations/postgres/17_dynamic_clients.sql
@@ -1,14 +1,15 @@
 create table clients_dyn
 (
-    id                 varchar not null
+    id                         varchar not null
         constraint clients_dyn_pk
             primary key
         constraint clients_dyn_clients_id_fk
             references clients
             on update cascade on delete cascade,
-    created            bigint  not null,
-    last_used          bigint,
-    registration_token bytea   not null
+    created                    bigint  not null,
+    last_used                  bigint,
+    registration_token         bytea   not null,
+    token_endpoint_auth_method varchar
 );
 
 create index clients_dyn_last_used_index
