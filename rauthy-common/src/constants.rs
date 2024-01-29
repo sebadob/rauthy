b1d2a96b7f22721ea3da956cde4e11fa68af88bd
sebadob <sebastiandobe@mailbox.org>
2024-01-29 11:55:59 +0100

    impl `DYN_CLIENT_DEFAULT_TOKEN_LIFETIME` via config

--- rauthy-common/src/constants.rs
+++ rauthy-common/src/constants.rs
@@ -132,6 +132,10 @@ lazy_static! {
         .parse::<bool>()
         .expect("ENABLE_DYN_CLIENT_REG cannot be parsed to bool - bad format");
     pub static ref DYN_CLIENT_REG_TOKEN: Option<String> = env::var("DYN_CLIENT_REG_TOKEN").ok();
+    pub static ref DYN_CLIENT_DEFAULT_TOKEN_LIFETIME: i32 = env::var("DYN_CLIENT_DEFAULT_TOKEN_LIFETIME")
+        .unwrap_or_else(|_| String::from("1800"))
+        .parse::<i32>()
+        .expect("DYN_CLIENT_DEFAULT_TOKEN_LIFETIME cannot be parsed to i64 - bad format");
 
     pub static ref ENABLE_EPHEMERAL_CLIENTS: bool = env::var("ENABLE_EPHEMERAL_CLIENTS")
         .unwrap_or_else(|_| String::from("false"))
