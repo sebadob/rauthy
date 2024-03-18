c6c39dfcbd725eae60a3370c75fd35bbe6571fa1
sd <sebastiandobe@mailbox.org>
2024-03-18 10:26:20 +0100

    `AuthProvider::create()`

--- rauthy-models/src/entity/auth_provider.rs
+++ rauthy-models/src/entity/auth_provider.rs
@@ -1,7 +1,12 @@
+use crate::app_state::AppState;
 use crate::entity::well_known::WellKnown;
+use crate::request::ProviderRequest;
 use crate::response::ProviderLookupResponse;
-use rauthy_common::constants::RAUTHY_VERSION;
+use actix_web::web;
+use rauthy_common::constants::{CACHE_NAME_12HR, IDX_AUTH_PROVIDER, RAUTHY_VERSION};
 use rauthy_common::error_response::{ErrorResponse, ErrorResponseType};
+use rauthy_common::utils::new_store_id;
+use redhac::{cache_insert, AckLevel};
 use reqwest::tls;
 use serde::{Deserialize, Serialize};
 use std::time::Duration;
@@ -19,7 +24,7 @@ pub struct AuthProvider {
 
     pub client_id: String,
     pub secret: Option<String>,
-    pub scope: Vec<String>,
+    pub scope: String,
 
     pub token_auth_method_basic: bool,
     pub use_pkce: bool,
@@ -30,6 +35,74 @@ pub struct AuthProvider {
 }
 
 impl AuthProvider {
+    pub async fn create(
+        data: &web::Data<AppState>,
+        payload: ProviderRequest,
+    ) -> Result<Self, ErrorResponse> {
+        let id = new_store_id();
+        let scope = payload
+            .scope
+            .iter()
+            .map(|s| s.trim())
+            .collect::<Vec<&str>>()
+            .join("+");
+
+        let slf = Self {
+            id,
+            name: payload.name,
+            issuer: payload.issuer,
+            authorization_endpoint: payload.authorization_endpoint,
+            token_endpoint: payload.token_endpoint,
+            userinfo_endpoint: payload.userinfo_endpoint,
+            client_id: payload.client_id,
+            secret: payload.secret,
+            scope,
+            token_auth_method_basic: payload.token_auth_method_basic,
+            use_pkce: payload.use_pkce,
+            root_pem: payload.root_pem,
+
+            // TODO when implemented
+            logo: None,
+            logo_type: None,
+        };
+
+        sqlx::query!(
+            r#"
+            INSERT INTO
+            auth_providers (id, name, issuer, authorization_endpoint, token_endpoint, userinfo_endpoint,
+            client_id, secret, scope, token_auth_method_basic, use_pkce, root_pem, logo, logo_type)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)"#,
+            slf.id,
+            slf.name,
+            slf.issuer,
+            slf.authorization_endpoint,
+            slf.token_endpoint,
+            slf.userinfo_endpoint,
+            slf.client_id,
+            slf.secret,
+            slf.scope,
+            slf.token_auth_method_basic,
+            slf.use_pkce,
+            slf.root_pem,
+            slf.logo,
+            slf.logo_type,
+        )
+        .execute(&data.db)
+        .await?;
+
+        // TODO
+        cache_insert(
+            CACHE_NAME_12HR.to_string(),
+            IDX_AUTH_PROVIDER.to_string(),
+            &data.caches.ha_cache_config,
+            &slf,
+            AckLevel::Quorum,
+        )
+        .await?;
+
+        Ok(slf)
+    }
+
     pub async fn lookup_config(
         issuer: &str,
         danger_allow_http: bool,
@@ -92,6 +165,10 @@ impl AuthProvider {
             authorization_endpoint: well_known.authorization_endpoint,
             token_endpoint: well_known.token_endpoint,
             userinfo_endpoint: well_known.userinfo_endpoint,
+            token_auth_method_basic: !well_known
+                .token_endpoint_auth_methods_supported
+                .iter()
+                .any(|m| m == "client_secret_post"),
             use_pkce: well_known
                 .code_challenge_methods_supported
                 .iter()
