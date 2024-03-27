206592d80e281c0664e9716b797d9eea6c7cc90a
sd <sebastiandobe@mailbox.org>
2024-03-27 09:56:51 +0100

    UI: provider HTML template

---
+++ frontend/src/routes/providers/callback/+page.svelte
@@ -0,0 +1,68 @@
+<script>
+    import Loading from "$lib/Loading.svelte";
+    import {
+        deleteVerifierFromStorage,
+        getQueryParams,
+        getVerifierFromStorage,
+        saveAccessToken,
+        saveCsrfToken,
+        saveIdToken
+    } from "../../../utils/helpers.js";
+    import {onMount} from "svelte";
+    import {CLIENT_ID, REDIRECT_URI_SUCCESS, REDIRECT_URI_SUCCESS_ACC} from "../../../utils/constants.js";
+    import {getSessionInfoXsrf, getToken} from "../../../utils/dataFetching.js";
+
+    onMount(async () => {
+        const query = getQueryParams();
+
+        const state = query.state;
+        const code = query.code;
+
+        // TODO instead of the normal callback, we do not want to get a token,
+        // but actually finish the provider login flow, which then will start with
+        // Rauthy's default login flow.
+
+        console.error('TODO implement callback logic');
+        return;
+
+        // const data = new URLSearchParams();
+        // let redirectUri = REDIRECT_URI_SUCCESS;
+        // if (query.state && query.state === 'account') {
+        //     redirectUri = REDIRECT_URI_SUCCESS_ACC;
+        // }
+        //
+        // data.append('grant_type', 'authorization_code');
+        // data.append('code', query.code);
+        // data.append('redirect_uri', redirectUri);
+        // data.append('client_id', CLIENT_ID);
+        // data.append('code_verifier', getVerifierFromStorage());
+        //
+        // // get and save the tokens
+        // let res = await getToken(data);
+        // let body = await res.json();
+        // // Save the access token in case we need to fetch a fresh CSRF token
+        // saveAccessToken(body.access_token);
+        // // ID Token is saved for the automatic logout with 'token_hint'
+        // saveIdToken(body.id_token);
+        //
+        // res = await getSessionInfoXsrf(body.access_token);
+        // body = await res.json();
+        // saveCsrfToken(body.csrf_token);
+        //
+        // // clean up
+        // deleteVerifierFromStorage();
+        //
+        // // all good -> redirect now
+        // window.location.href = redirectUri;
+    });
+
+    //
+</script>
+
+<svelte:head>
+    <title>Callback</title>
+</svelte:head>
+
+<Loading/>
+
+<!-- Placeholder for SSR of provider callback errors -->
