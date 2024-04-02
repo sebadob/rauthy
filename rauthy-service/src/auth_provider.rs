db6130d57ea16fa7f7c9cd6ca0acb469531206d4
sd <sebastiandobe@mailbox.org>
2024-04-02 12:31:04 +0200

    update TODO

--- rauthy-service/src/auth_provider.rs
+++ rauthy-service/src/auth_provider.rs
@@ -57,13 +57,13 @@
 // - [x] custom validation check for providers to either be confidential or support pkce
 //
 // TODO:
-// - [ ] Show additional information in each users account page, if it is a federated account
+// - [x] Show additional information in each users account page, if it is a federated account
 // - [ ] i18n for the auth provider callback page, especially the different error cases
 // - [ ] Define mandatory mechanisms and features, that external providers must support to make
 // everything working securely and write these down in the Rauthy book (new section)
-// - [ ] Fix the new UI bug in the account page if the account does not have any password + passkey
+// - [x] Fix the new UI bug in the account page if the account does not have any password + passkey
 // -> page is not rendering currently
-// - [ ] Make it possible to create a Password / Passkey for federated accounts, so this flow may
+// - [x] Make it possible to create a Password / Passkey for federated accounts, so this flow may
 // be used for the initial setup only and use Rauthy's advanced security mechanisms afterwards.
 // - [ ] Make it possible to upload a small logo or icon for providers?
 // What about the image size? Should Rauthy actually process and optimize it or "just take it" and
