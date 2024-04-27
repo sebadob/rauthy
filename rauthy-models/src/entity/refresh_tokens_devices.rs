a7d41c8746877a974204f2e44d26d51e1e0f4755
sd <sebastiandobe@mailbox.org>
2024-04-27 13:37:43 +0200

    improve refresh token invalidation and do it via sql query

--- rauthy-models/src/entity/refresh_tokens_devices.rs
+++ rauthy-models/src/entity/refresh_tokens_devices.rs
@@ -4,38 +4,36 @@ use chrono::{DateTime, Utc};
 use rauthy_common::error_response::{ErrorResponse, ErrorResponseType};
 use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
+use time::OffsetDateTime;
 
 #[derive(Debug, FromRow, Serialize, Deserialize)]
-pub struct RefreshToken {
+pub struct RefreshTokenDevice {
     pub id: String,
+    pub device_id: String,
     pub user_id: String,
     pub nbf: i64,
     pub exp: i64,
     pub scope: Option<String>,
-    pub is_mfa: bool,
 }
 
 // CRUD
-impl RefreshToken {
+impl RefreshTokenDevice {
     pub async fn create(
         data: &web::Data<AppState>,
         id: String,
+        device_id: String,
         user_id: String,
         nbf: DateTime<Utc>,
         exp: DateTime<Utc>,
         scope: Option<String>,
-        // TODO should we even save mfa for refresh tokens?
-        // even if the original token has been issued with mfa, the refresh
-        // token not really is, because it can be given without user interaction.
-        is_mfa: bool,
     ) -> Result<Self, ErrorResponse> {
         let rt = Self {
             id,
+            device_id,
             user_id,
             nbf: nbf.timestamp(),
             exp: exp.timestamp(),
             scope,
-            is_mfa,
         };
 
         rt.save(data).await?;
@@ -43,25 +41,27 @@ impl RefreshToken {
     }
 
     pub async fn delete(&self, data: &web::Data<AppState>) -> Result<(), ErrorResponse> {
-        sqlx::query!("DELETE FROM refresh_tokens WHERE id = $1", self.id)
+        sqlx::query!("DELETE FROM refresh_tokens_devices WHERE id = $1", self.id)
             .execute(&data.db)
             .await?;
         Ok(())
     }
 
     pub async fn find_all(data: &web::Data<AppState>) -> Result<Vec<Self>, ErrorResponse> {
-        let res = sqlx::query_as!(Self, "SELECT * FROM refresh_tokens")
+        let res = sqlx::query_as!(Self, "SELECT * FROM refresh_tokens_devices")
             .fetch_all(&data.db)
             .await?;
         Ok(res)
     }
 
     pub async fn invalidate_all(data: &web::Data<AppState>) -> Result<(), ErrorResponse> {
-        let now = Utc::now().timestamp();
-
-        sqlx::query!("UPDATE refresh_tokens SET exp = $1 WHERE exp > $1", now,)
-            .execute(&data.db)
-            .await?;
+        let now = OffsetDateTime::now_utc().unix_timestamp();
+        sqlx::query!(
+            "UPDATE refresh_tokens_devices SET exp = $1 WHERE exp > $1",
+            now
+        )
+        .execute(&data.db)
+        .await?;
 
         Ok(())
     }
@@ -73,7 +73,7 @@ impl RefreshToken {
         let now = Utc::now().timestamp();
 
         sqlx::query!(
-            "UPDATE refresh_tokens SET exp = $1 WHERE exp > $1 AND user_id = $2",
+            "UPDATE refresh_tokens_devices SET exp = $1 WHERE exp > $1 AND user_id = $2",
             now,
             user_id
         )
@@ -84,57 +84,79 @@ impl RefreshToken {
     }
 
     pub async fn find(data: &web::Data<AppState>, id: &str) -> Result<Self, ErrorResponse> {
-        match sqlx::query_as!(Self, "SELECT * FROM refresh_tokens WHERE id = $1", id)
-            .fetch_one(&data.db)
-            .await
+        match sqlx::query_as!(
+            Self,
+            "SELECT * FROM refresh_tokens_devices WHERE id = $1",
+            id
+        )
+        .fetch_one(&data.db)
+        .await
         {
             Ok(res) => Ok(res),
             Err(_) => Err(ErrorResponse::new(
                 ErrorResponseType::NotFound,
-                "Refresh Token does not exist".to_string(),
+                "Device Refresh Token does not exist".to_string(),
             )),
         }
+    }
+
+    pub async fn invalidate_all_for_device(
+        data: &web::Data<AppState>,
+        device_id: &str,
+    ) -> Result<(), ErrorResponse> {
+        sqlx::query!(
+            "DELETE FROM refresh_tokens_devices WHERE device_id = $1",
+            device_id
+        )
+        .execute(&data.db)
+        .await?;
+
+        Ok(())
+    }
+
+    pub async fn invalidate_all_for_user(
+        data: &web::Data<AppState>,
+        user_id: &str,
+    ) -> Result<(), ErrorResponse> {
+        sqlx::query!(
+            "DELETE FROM refresh_tokens_devices WHERE user_id = $1",
+            user_id
+        )
+        .execute(&data.db)
+        .await?;
+
+        Ok(())
     }
 
     pub async fn save(&self, data: &web::Data<AppState>) -> Result<(), ErrorResponse> {
         #[cfg(not(feature = "postgres"))]
         let q = sqlx::query!(
-            r#"INSERT OR REPLACE INTO refresh_tokens (id, user_id, nbf, exp, scope, is_mfa)
+            r#"INSERT OR REPLACE INTO refresh_tokens_devices
+                (id, device_id, user_id, nbf, exp, scope)
                 VALUES ($1, $2, $3, $4, $5, $6)"#,
             self.id,
+            self.device_id,
             self.user_id,
             self.nbf,
             self.exp,
             self.scope,
-            self.is_mfa,
         );
         #[cfg(feature = "postgres")]
         let q = sqlx::query!(
-            r#"INSERT INTO refresh_tokens (id, user_id, nbf, exp, scope, is_mfa)
+            r#"INSERT INTO refresh_tokens_devices
+                (id, device_id, user_id, nbf, exp, scope)
                 VALUES ($1, $2, $3, $4, $5, $6)
-                ON CONFLICT(id) DO UPDATE SET user_id = $2, nbf = $3, exp = $4, scope = $5"#,
+                ON CONFLICT(id) DO UPDATE
+                SET device_id = $2, user_id = $2, nbf = $3, exp = $4, scope = $5"#,
             self.id,
+            self.device_id,
             self.user_id,
             self.nbf,
             self.exp,
             self.scope,
-            self.is_mfa,
         );
 
         q.execute(&data.db).await?;
-
-        Ok(())
-    }
-}
-
-impl RefreshToken {
-    pub async fn invalidate_all_for_user(
-        data: &web::Data<AppState>,
-        id: &str,
-    ) -> Result<(), ErrorResponse> {
-        sqlx::query!("DELETE FROM refresh_tokens WHERE user_id = $1", id)
-            .execute(&data.db)
-            .await?;
 
         Ok(())
     }
