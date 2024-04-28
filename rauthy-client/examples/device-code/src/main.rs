c9453b2b394dc8aa01e457b268d8a491e365c9b7
sd <sebastiandobe@mailbox.org>
2024-04-28 15:15:16 +0200

    client: create an example for `device-code`

--- rauthy-client/examples/device-code/src/main.rs
+++ rauthy-client/examples/device-code/src/main.rs
@@ -1,6 +1,6 @@
 use rauthy_client::device_code::DeviceCode;
 use rauthy_client::{DangerAcceptInvalidCerts, RauthyHttpsOnly};
-use tracing::{info, subscriber, Level};
+use tracing::{subscriber, Level};
 use tracing_subscriber::FmtSubscriber;
 
 #[tokio::main]
@@ -10,17 +10,24 @@ async fn main() -> anyhow::Result<()> {
         .finish();
     subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
-    let device_code = DeviceCode::request_with(
+    let mut device_code = DeviceCode::request_with(
         "http://localhost:8080",
-        "device",
-        None,
+        "device".to_string(),
+        Some("ZEW3dyKiPG27LdWrOFkNg6m9CaAEW5beoCxSufwdEBzSXMsxjNAXsnEbeI074d4V".to_string()),
         None,
         None,
         RauthyHttpsOnly::No,
         DangerAcceptInvalidCerts::Yes,
     )
     .await?;
-    info!("{:?}", device_code);
+    println!("{}", device_code);
+
+    let _ts = device_code.wait_for_token().await?;
+
+    // If the request has been verified, we have received a TokenSet at this point,
+    // which we can use for furter requests.
+
+    // TODO complete the example with qr code generation and fetch_userinfo()
 
     Ok(())
 }
