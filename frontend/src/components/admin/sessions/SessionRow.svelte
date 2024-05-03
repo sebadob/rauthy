b8dd33685361ff6eb9b6c01ba889c0d6de9f27b9
sd <sebastiandobe@mailbox.org>
2024-05-03 16:05:06 +0200

    UI: prettify the sessions view a little bit

--- frontend/src/components/admin/sessions/SessionRow.svelte
+++ frontend/src/components/admin/sessions/SessionRow.svelte
@@ -1,83 +1,102 @@
 <script>
     import {formatDateFromTs} from "../../../utils/helpers.js";
     import CheckIcon from "$lib/CheckIcon.svelte";
+    import ExpandContainer from "$lib/ExpandContainer.svelte";
+    import Tooltip from "$lib/Tooltip.svelte";
 
     export let session = {};
     export let now = 0;
 </script>
 
-<div class={session.exp > now ? 'entryRow' : 'entryRow expired'}>
-    <div class="row1">
-        <div class="col-sid flex font-mono">
-            <div class="label" style:margin-right=".75rem">SID:</div>
-            {session.id}
+<div class="container" class:expired={session.exp < now}>
+    <ExpandContainer>
+        <div class="header" slot="header">
+            <Tooltip text="User ID">
+                <div class="uid">
+                    {session.user_id}
+                </div>
+            </Tooltip>
+
+            <div class="header">
+                <Tooltip text="Peer IP">
+                    <div class="ip">
+                        {session.remote_ip}
+                    </div>
+                </Tooltip>
+
+                <Tooltip text="Last Seen">
+                    <div class="date">
+                        {formatDateFromTs(session.last_seen)}
+                    </div>
+                </Tooltip>
+            </div>
         </div>
 
-        <div class="col-exp flex">
-            <div class="label">EXP:</div>
-            {formatDateFromTs(session.exp)}
-        </div>
+        <div class="details" slot="body">
+            <div class="flex">
+                <div class="label">Session ID:</div>
+                {session.id}
+            </div>
 
-        <div class="col-seen flex">
-            <div class="label">SEEN:</div>
-            {formatDateFromTs(session.last_seen)}
-        </div>
-    </div>
+            <div class="flex">
+                <div class="label">User ID:</div>
+                {session.user_id}
+            </div>
 
-    <div class="row2">
-        <div class="col-uid flex font-mono">
-            <div class="label">USER:</div>
-            {session.user_id}
-        </div>
+            <div class="flex">
+                <div class="label">Expires:</div>
+                {formatDateFromTs(session.exp)}
+            </div>
 
-        <div class="col-state flex">
-            <div class="label">STATE:</div>
-            {session.state}
-        </div>
+            <div class="flex">
+                <div class="label">Last Seen:</div>
+                {formatDateFromTs(session.last_seen)}
+            </div>
 
-        <div class="col-ip flex">
-            <div class="label">IP:</div>
-            {session.remote_ip}
-        </div>
+            <div class="flex">
+                <div class="label">State:</div>
+                {session.state}
+            </div>
 
-        <div class="col-mfa flex">
-            <div class="label">MFA:</div>
-            <CheckIcon check={session.is_mfa}/>
+            <div class="flex">
+                <div class="label">IP:</div>
+                {session.remote_ip}
+            </div>
+
+            <div class="flex">
+                <div class="label">MFA:</div>
+                <CheckIcon check={session.is_mfa}/>
+            </div>
         </div>
-    </div>
+    </ExpandContainer>
 </div>
 
 <style>
-    /*.button {*/
-    /*    margin: 10px 0 0 25px;*/
-    /*}*/
-
-    .col-sid {
-        width: 23rem;
+    .container {
+        max-width: 48.5rem;
     }
 
-    .col-uid {
-        width: 23rem;
+    .header {
+        display: flex;
+        flex-wrap: wrap;
     }
 
-    .col-state {
-        width: 12.25rem;
+    .uid {
+        min-width: 14rem;
     }
 
-    .col-mfa {
-        width: 4.5rem;
+    .ip {
+        width: 8rem;
+        text-align: right;
     }
 
-    .col-exp {
-        width: 12.25rem;
+    .date {
+        width: 10rem;
+        text-align: right;
     }
 
-    .col-seen {
-        width: 13rem;
-    }
-
-    .col-ip {
-        width: 9.75rem;
+    .details {
+        padding: 1rem;
     }
 
     .expired {
@@ -90,24 +109,9 @@
     }
 
     .label {
+        width: 6rem;
         margin-right: .25rem;
         font-weight: bold;
         font-size: .9rem;
-    }
-
-    .entryRow {
-        max-width: 50rem;
-        margin-bottom: .5rem;
-    }
-
-    .entryRow:hover {
-        background: var(--col-acnt);
-        color: white
-    }
-
-    .row1, .row2 {
-        display: inline-flex;
-        flex-wrap: wrap;
-        flex: 1;
     }
 </style>
