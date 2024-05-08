8330fafbb9658bcd6132b1dd82836406ea3ccec1
sd <sebastiandobe@mailbox.org>
2024-05-08 13:52:51 +0200

    client: update the `device-code` example and show refresh token handler usage

--- rauthy-client/examples/device-code/src/main.rs
+++ rauthy-client/examples/device-code/src/main.rs
@@ -1,12 +1,23 @@
 use rauthy_client::device_code::DeviceCode;
 use rauthy_client::rauthy_error::RauthyError;
+use rauthy_client::token_set::OidcTokenSet;
 use rauthy_client::{DangerAcceptInvalidCerts, RauthyHttpsOnly};
+use tracing::{subscriber, Level};
+use tracing_subscriber::FmtSubscriber;
 
 #[tokio::main]
 async fn main() -> Result<(), RauthyError> {
+    // the rauthy-client emits some tracing output
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .finish();
+    subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+    let client_id = "device".to_string();
+
     let mut device_code = DeviceCode::request_with(
         "http://localhost:8080",
-        "device".to_string(),
+        client_id.clone(),
         None,
         None,
         None,
@@ -36,6 +47,21 @@ async fn main() -> Result<(), RauthyError> {
 
     let claims = ts.id_claims()?.unwrap();
     println!("\nWe get the user claims as well:\n{:?}", claims);
+
+    // If the client is configured to receive a refresh token, we can
+    // spawn a refresh handler that does all the magic behind the scenes.
+    // This will consume the current OidcTokenSet.
+    ts.into_refresh_handler_with(
+        client_id,
+        None,
+        None,
+        RauthyHttpsOnly::No,
+        DangerAcceptInvalidCerts::Yes,
+    )
+    .await?;
+    // After spawning it, you can get the currently saved tokens with
+    let _access_token = OidcTokenSet::access_token().await?;
+    let _id_token = OidcTokenSet::id_token().await?;
 
     Ok(())
 }
