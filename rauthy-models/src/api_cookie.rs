4ba42be7bbd4a7d0f94dcd75dcc7721a8f7f2437
sd <sebastiandobe@mailbox.org>
2024-05-17 16:18:29 +0200

    encrypt any `ApiCookie` by default

--- rauthy-models/src/api_cookie.rs
+++ rauthy-models/src/api_cookie.rs
@@ -1,7 +1,10 @@
 use actix_web::cookie::{Cookie, SameSite};
 use actix_web::dev::ServiceRequest;
 use actix_web::{cookie, HttpRequest};
+use cryptr::EncValue;
 use rauthy_common::constants::{CookieMode, COOKIE_MODE, COOKIE_SET_PATH};
+use rauthy_common::utils::{base64_decode, base64_encode};
+use semver::Op;
 use std::borrow::Cow;
 use std::fmt::Display;
 use tracing::warn;
@@ -9,10 +12,10 @@ use tracing::warn;
 pub struct ApiCookie;
 
 impl ApiCookie {
-    pub fn build<'c, N, V>(name: N, value: V, max_age: i64) -> Cookie<'c>
+    pub fn build<'c, 'b, N, V>(name: N, value: V, max_age: i64) -> Cookie<'c>
     where
         N: Into<Cow<'c, str>> + Display,
-        V: Into<Cow<'c, str>> + Display,
+        V: Into<Cow<'b, str>> + Display,
     {
         let path = if *COOKIE_SET_PATH { "/auth" } else { "/" };
         let (name, secure, path) = match *COOKIE_MODE {
@@ -29,7 +32,12 @@ impl ApiCookie {
             cookie::time::Duration::seconds(max_age)
         };
 
-        Cookie::build(name, value)
+        // we always encrypt any cookie value
+        let enc =
+            EncValue::encrypt(value.into().as_bytes()).expect("ENC_VALUES not set up correctly");
+        let value_b64 = base64_encode(enc.into_bytes().as_ref());
+
+        Cookie::build(name, value_b64)
             .secure(secure)
             .http_only(true)
             .same_site(SameSite::Lax)
@@ -38,7 +46,7 @@ impl ApiCookie {
             .finish()
     }
 
-    pub fn from_req<'c, N>(req: &HttpRequest, cookie_name: N) -> Option<Cookie<'c>>
+    pub fn from_req<'c, N>(req: &HttpRequest, cookie_name: N) -> Option<String>
     where
         N: Into<Cow<'c, str>> + Display,
     {
@@ -47,10 +55,11 @@ impl ApiCookie {
             CookieMode::Secure => format!("__Secure-{}", cookie_name),
             CookieMode::DangerInsecure => cookie_name.to_string(),
         };
-        req.cookie(&name)
+        // req.cookie(&name)
+        Self::cookie_into_value(req.cookie(&name))
     }
 
-    pub fn from_svc_req<'c, N>(req: &ServiceRequest, cookie_name: N) -> Option<Cookie<'c>>
+    pub fn from_svc_req<'c, N>(req: &ServiceRequest, cookie_name: N) -> Option<String>
     where
         N: Into<Cow<'c, str>> + Display,
     {
@@ -59,6 +68,38 @@ impl ApiCookie {
             CookieMode::Secure => format!("__Secure-{}", cookie_name),
             CookieMode::DangerInsecure => cookie_name.to_string(),
         };
-        req.cookie(&name)
+        Self::cookie_into_value(req.cookie(&name))
+    }
+
+    pub fn cookie_into_value(cookie: Option<Cookie>) -> Option<String> {
+        match cookie {
+            None => None,
+            Some(cookie) => {
+                let val = cookie.value();
+                let bytes = base64_decode(val).ok()?;
+                let enc = EncValue::try_from(bytes).ok()?;
+                let dec = enc.decrypt().ok()?;
+                Some(String::from_utf8_lossy(dec.as_ref()).to_string())
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use cryptr::EncKeys;
+
+    #[test]
+    fn test_api_cookie() {
+        dotenvy::from_filename("rauthy.test.cfg").ok();
+        let _ = EncKeys::from_env().unwrap().init();
+
+        let val = "my_cookie_val_1337";
+
+        let cookie = ApiCookie::build("myCookie", val, 10);
+        let val_from = ApiCookie::cookie_into_value(Some(cookie)).unwrap();
+
+        assert_eq!(val, val_from);
     }
 }
