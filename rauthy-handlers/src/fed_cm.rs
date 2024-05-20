62df41a988effae14b32150aaa46f726165adeef
sd <sebastiandobe@mailbox.org>
2024-05-20 11:12:09 +0200

    `FedCMAccounts` endpoint

--- rauthy-handlers/src/fed_cm.rs
+++ rauthy-handlers/src/fed_cm.rs
@@ -2,11 +2,13 @@ use actix_web::http::header;
 use actix_web::http::header::{HeaderValue, CONTENT_TYPE};
 use actix_web::{get, web, HttpRequest, HttpResponse};
 use rauthy_common::constants::{
-    APPLICATION_JSON, EXPERIMENTAL_FED_CM_ENABLE, HEADER_ALLOW_ALL_ORIGINS, HEADER_JSON,
+    APPLICATION_JSON, COOKIE_FED_CM, EXPERIMENTAL_FED_CM_ENABLE, HEADER_ALLOW_ALL_ORIGINS,
+    HEADER_JSON,
 };
 use rauthy_common::error_response::{ErrorResponse, ErrorResponseType};
+use rauthy_models::api_cookie::ApiCookie;
 use rauthy_models::app_state::AppState;
-use rauthy_models::entity::fed_cm::{FedCMIdPConfig, WebIdentity};
+use rauthy_models::entity::fed_cm::{FedCMAccounts, FedCMIdPConfig, WebIdentity};
 use rauthy_models::entity::well_known::WellKnown;
 
 /// GET accounts linked to the users
@@ -21,10 +23,30 @@ use rauthy_models::entity::well_known::WellKnown;
     ),
 )]
 #[get("/fed_cm/accounts")]
-pub async fn get_fed_cm_accounts(data: web::Data<AppState>) -> Result<HttpResponse, ErrorResponse> {
+pub async fn get_fed_cm_accounts(
+    req: HttpRequest,
+    data: web::Data<AppState>,
+) -> Result<HttpResponse, ErrorResponse> {
     is_fed_cm_enabled()?;
+    is_web_identity_fetch(&req)?;
 
-    todo!()
+    // The FedCM endpoints use a special cookie instead of the usual session.
+    // This is important to be able to recognize a user for a long period of time even whe
+    // there is no active / valid session right now. The Rauthy session cookie though has a very
+    // short lifetime, which is why it can't be re-used.
+    let user_id = ApiCookie::from_req(&req, COOKIE_FED_CM).ok_or_else(|| {
+        // CAUTION: The spec does not have any clarification on how to behave if the user
+        // is not logged in or does not exist
+        // -> we assume and use the OAuth2 WWW-Authenticate header for now
+        // https://github.com/fedidcg/FedCM/issues/218
+        ErrorResponse::new(
+            ErrorResponseType::WWWAuthenticate("user-not-found".to_string()),
+            "No user found by FedCM cookie".to_string(),
+        )
+    })?;
+
+    let accounts = FedCMAccounts::try_for_user_id(&data, user_id).await?;
+    Ok(HttpResponse::Ok().json(accounts))
 }
 
 /// GET metadata for the FedCM client
@@ -117,7 +139,7 @@ pub async fn post_fed_cm_token(data: web::Data<AppState>) -> Result<HttpResponse
 ///
 /// TODO from the spec, this MUST be at eTLD+1:
 /// -> not compatible with OIDC spec (<issuer>/.well-known/...)
-/// -> mention in docs!
+/// -> mention in docs - we must break out of `/auth` sub path
 #[utoipa::path(
     get,
     path = "/.well-known/web-identity",
--- rauthy-models/src/entity/fed_cm.rs
+++ rauthy-models/src/entity/fed_cm.rs
@@ -1,13 +1,96 @@
 use crate::app_state::AppState;
 use crate::entity::colors::ColorEntity;
+use crate::entity::users::User;
 use actix_web::web;
 use rauthy_common::constants::EMAIL_SUB_PREFIX;
-use rauthy_common::error_response::ErrorResponse;
+use rauthy_common::error_response::{ErrorResponse, ErrorResponseType};
 use serde::Serialize;
 use std::sync::OnceLock;
 use utoipa::ToSchema;
 
 static IDP_CONFIG: OnceLock<FedCMIdPConfig> = OnceLock::new();
+
+#[derive(Clone, Debug, Serialize, ToSchema)]
+pub struct FedCMAccount {
+    pub id: String,
+    // The user’s full name.
+    pub name: String,
+    pub email: String,
+    pub given_name: Option<String>,
+    // URL for the account’s picture.
+    pub picture: Option<String>,
+    // A list of RPs (that gets matched against the requesting clientId) this account is already
+    // registered with. Used in the request permission to sign-up to allow the IDP to control
+    // whether to show the Privacy Policy and the Terms of Service.
+    pub approved_clients: Vec<String>,
+    // A list of strings which correspond to all of the login hints which match with this account.
+    // An RP can use the loginHint to request that only an account matching a given value is shown
+    // to the user.
+    pub login_hints: Vec<String>,
+    // A list of strings which correspond to all of the domain hints which match with this account.
+    // An RP can use the domainHint to request that only an account matching a given value or
+    // containing some domain hint is shown to the user.
+    pub domain_hints: Vec<String>,
+}
+
+impl From<User> for FedCMAccount {
+    fn from(user: User) -> Self {
+        let name = format!("{} {}", user.given_name, user.family_name);
+        let login_hint = format!("login_hint={}", user.email);
+
+        Self {
+            id: user.id,
+            name,
+            email: user.email,
+            given_name: Some(user.given_name),
+            // Rauthy does not store user pictures
+            picture: None,
+            // TODO
+            approved_clients: vec![],
+            login_hints: vec![login_hint],
+            // Rauthy does not use such a value
+            domain_hints: vec![],
+        }
+    }
+}
+
+#[derive(Clone, Debug, Serialize, ToSchema)]
+pub struct FedCMAccounts {
+    pub accounts: Vec<FedCMAccount>,
+}
+
+impl FedCMAccounts {
+    pub async fn try_for_user_id(
+        data: &web::Data<AppState>,
+        user_id: String,
+    ) -> Result<Self, ErrorResponse> {
+        // We will stick to the WWW-Authenticate header for now and use duplicated code from
+        // some OAuth2 handlers for now until the spec has settled on an error behavior.
+        let user = User::find(data, user_id).await.map_err(|_| {
+            ErrorResponse::new(
+                ErrorResponseType::WWWAuthenticate("user-not-found".to_string()),
+                "The user has not been found".to_string(),
+            )
+        })?;
+
+        // reject the request if user has been disabled, even when the token is still valid
+        if !user.enabled || user.check_expired().is_err() {
+            return Err(ErrorResponse::new(
+                ErrorResponseType::WWWAuthenticate("user-disabled".to_string()),
+                "The user has been disabled".to_string(),
+            ));
+        }
+
+        // TODO does it make sense to store a dedicated FedCM session during login (if not exists)
+        // to be able to do remote-logouts and track them?
+        // -> probably something with a future minor version, since it would need a DB migration
+
+        let account = FedCMAccount::from(user);
+        Ok(Self {
+            accounts: vec![account],
+        })
+    }
+}
 
 #[derive(Clone, Debug, Serialize, ToSchema)]
 pub struct FedCMIdPIcon {
@@ -19,8 +102,8 @@ pub struct FedCMIdPIcon {
 impl FedCMIdPIcon {
     fn rauthy_logo(issuer: &str) -> Self {
         Self {
-            // TODO this will return an SVG by default -> check if okay
             url: format!("{}/clients/rauthy/logo", issuer),
+            // Rauthy's default icon is an SVG which is fine according to the spec -> no size
             size: None,
         }
     }
@@ -83,22 +166,6 @@ impl FedCMIdPConfig {
 
         let _ = IDP_CONFIG.set(slf);
         Ok(IDP_CONFIG.get().unwrap())
-
-        // let branding = FedCMIdPBranding::new(data).await?;
-        //
-        // let iss = &data.issuer;
-        // let sub_path = "fed_cm";
-        //
-        // // TODO we could build all of this only once and then use a cached response each time
-        // Ok(Self {
-        //     accounts_endpoint: format!("{}/{}/accounts", iss, sub_path),
-        //     client_metadata_endpoint: format!("{}/{}/client_meta", iss, sub_path),
-        //     id_assertion_endpoint: format!("{}/{}/token", iss, sub_path),
-        //     // TODO where should be point this URL in case of Rauthy for it to make sense?
-        //     login_url: format!("{}/{}/login", iss, sub_path),
-        //     disconnect_endpoint: format!("{}/{}/disconnect", iss, sub_path),
-        //     branding,
-        // })
     }
 }
 
