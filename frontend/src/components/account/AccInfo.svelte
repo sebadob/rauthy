22751ee6e9b31361d2ee5047a8c8795518d30745
sd <sebastiandobe@mailbox.org>
2024-05-25 21:27:55 +0200

    fix auth provider json parsing

--- frontend/src/components/account/AccInfo.svelte
+++ frontend/src/components/account/AccInfo.svelte
@@ -31,7 +31,10 @@
         //     "id": "7F6N7fb3el3P5XimjJSaeD2o",
         //     "name": "Rauthy IAM"
         // }];
-        providersAvailable = document?.getElementsByTagName("template").namedItem("auth_providers")?.innerHTML;
+        let tpl = JSON.parse(document?.getElementsByTagName("template").namedItem("auth_providers")?.innerHTML);
+        if (tpl) {
+            providersAvailable = JSON.parse(tpl);
+        }
     })
 
     function linkProvider(id) {
