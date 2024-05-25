83fce86bdd706d632f104781588a096171017856
sd <sebastiandobe@mailbox.org>
2024-05-25 11:27:24 +0200

    more information and logging in the ui

--- frontend/src/routes/fedcm/+page.svelte
+++ frontend/src/routes/fedcm/+page.svelte
@@ -1,11 +1,29 @@
 <script>
     import {onMount} from "svelte";
     import {getKey} from "$lib/utils/helpers.js";
+    import Button from "$lib/Button.svelte";
+    import Input from "$lib/inputs/Input.svelte";
+    import * as yup from "yup";
+    import {REGEX_CLIENT_ID, REGEX_CLIENT_NAME, REGEX_URI} from "../../utils/constants.js";
+    import {extractFormErrors} from "../../utils/helpers.js";
+    import CheckIcon from "$lib/CheckIcon.svelte";
 
     let configUrl = '';
     let isSupported = false;
     let isLoggedIn = false;
     let credentials = '';
+
+    $: console.log('built config url: ' + configUrl);
+
+    let formValues = {
+        clientId: 'fedcm',
+        configUrl: 'any',
+    }
+    let formErrors = {};
+    const schema = yup.object().shape({
+        clientId: yup.string().required('Client ID is required').trim().matches(REGEX_URI, "Must be URL safe"),
+        configUrl: yup.string().nullable().trim().matches(REGEX_URI, "Must be URL safe"),
+    });
 
     onMount(async () => {
         configUrl = `${window.location.origin}/auth/v1/fed_cm/config`;
@@ -19,53 +37,96 @@
             console.error('FedCM is not supported');
             return;
         }
-
-        // console.log(IdentityProvider);
-        // let state = IdentityProvider.getState();
-        // console.log(state);
     });
 
     async function login() {
+        let isValid = await validateForm();
+        if (!isValid) {
+            return;
+        }
+
+        let configUrl = formValues.configUrl || undefined;
+        let clientId = formValues.clientId;
+        console.log('using credentials get values: configUrl: ' + configUrl + ' / clientId: ' + clientId);
+
         try {
-            credentials = await navigator.credentials.get({
+            let creds = await navigator.credentials.get({
                 identity: {
                     context: "signin",
                     providers: [{
-                        configURL: "any",
-                        clientId: "fedcm",
+                        configURL: configUrl,
+                        // TODO configurable via input
+                        clientId: clientId,
                         nonce: getKey(48),
                     }]
                 }
             })
+            console.log(creds);
+            console.log(creds.id);
+            console.log(creds.type);
+            console.log(creds.token);
+            credentials = creds.token || 'logged in';
+            isLoggedIn = true;
         } catch (err) {
-            console.log('FedCM credentials error: ' + err);
+            console.error('FedCM credentials error: ' + err);
         }
     }
 
     function register() {
-        console.log('Rauthy FedCM configUrl: ' + configUrl);
         IdentityProvider.register(configUrl);
+    }
+
+    async function validateForm() {
+        try {
+            await schema.validate(formValues, {abortEarly: false});
+            formErrors = {};
+            return true;
+        } catch (err) {
+            formErrors = extractFormErrors(err);
+            return false;
+        }
     }
 </script>
 
 <h1>FedCM Testing</h1>
 <p>
     <b>FedCM supported:</b>
-    {#if isSupported}
-        yes
-    {:else}
-        no -> enable idp registration:<br>
+    <CheckIcon check={isSupported}/>
+    {#if !isSupported}
+        <br>-> enable idp registration:<br>
         <code>chrome://flags/#fedcm-idp-registration</code>
     {/if}
 </p>
 
 {#if isSupported}
     <p>
-        <button on:click={register}>Register IdP</button>
+        <Button on:click={register} level={3}>Register IdP</Button>
     </p>
 
     <p>
-        <button on:click={login}>Login</button>
+        <Input
+                bind:value={formValues.clientId}
+                bind:error={formErrors.clientId}
+                autocomplete="off"
+                placeholder="Client Id"
+                on:input={validateForm}
+        >
+            CLIENT ID
+        </Input>
+        <Input
+                bind:value={formValues.configUrl}
+                bind:error={formErrors.configUrl}
+                autocomplete="off"
+                placeholder="Config URL"
+                on:input={validateForm}
+        >
+            CONFIG URL
+        </Input>
+        <Button on:click={login}>Login</Button>
+    </p>
+    <p>
+        <b>Logged In:</b>
+        <CheckIcon check={isLoggedIn}/>
     </p>
 {/if}
 
