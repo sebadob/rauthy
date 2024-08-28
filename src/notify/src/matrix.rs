23a9fbeab0301fa64610ae1b94181e62b74e367f
sebadob <sebastiandobe@mailbox.org>
2024-08-28 10:07:08 +0200

    migrate matrix notifier to `ruma`

--- src/notify/src/matrix.rs
+++ src/notify/src/matrix.rs
@@ -1,13 +1,12 @@
 use async_trait::async_trait;
-use matrix_sdk::config::SyncSettings;
-use matrix_sdk::matrix_auth::{MatrixSession, MatrixSessionTokens};
-use matrix_sdk::ruma::events::room::message::RoomMessageEventContent;
-use matrix_sdk::ruma::{OwnedDeviceId, OwnedRoomId, OwnedUserId};
-use matrix_sdk::{AuthSession, Client, RoomState, SessionMeta};
 use rauthy_common::constants::RAUTHY_VERSION;
 use rauthy_error::{ErrorResponse, ErrorResponseType};
+use ruma::client::http_client;
+use ruma::events::room::message::RoomMessageEventContent;
+use ruma::{OwnedDeviceId, OwnedRoomId, OwnedUserId, TransactionId};
 use std::str::FromStr;
 use std::time::Duration;
+use tokio::sync::Mutex;
 use tokio::time;
 use tracing::{debug, error, info, warn};
 
@@ -17,14 +16,17 @@ use crate::{Notification, Notify};
 pub struct NotifierMatrix {
     user_id: OwnedUserId,
     room_id: OwnedRoomId,
-    access_token: Option<String>,
     user_password: Option<String>,
     device_id: OwnedDeviceId,
-    client: Client,
+
+    client: ruma::Client<http_client::Reqwest>,
+    access_token: Mutex<Option<String>>,
+    refresh_token: Mutex<Option<String>>,
 }
 
 impl NotifierMatrix {
     pub async fn try_new(
+        server_url: String,
         user_id: &str,
         room_id: &str,
         access_token: Option<String>,
@@ -49,43 +51,37 @@ impl NotifierMatrix {
         let rauthy_notifier = format!("Rauthy v{} Notifier", RAUTHY_VERSION);
         let device_id = OwnedDeviceId::from(rauthy_notifier.as_str());
 
-        let http_client =
-            Notification::build_client_matrix(disable_tls_validation, root_ca_path).await;
-        let client = Client::builder()
-            .server_name(user_id.server_name())
-            .user_agent(rauthy_notifier)
-            .handle_refresh_tokens()
+        let http_client = Notification::build_client(disable_tls_validation, root_ca_path).await;
+
+        let client = ruma::Client::builder()
+            .homeserver_url(server_url.to_string())
+            .access_token(access_token.clone())
             .http_client(http_client)
-            .build()
-            .await
-            .map_err(|err| {
-                ErrorResponse::new(
-                    ErrorResponseType::Internal,
-                    format!("Cannot build Matrix Notification Client: {:?}", err),
-                )
-            })?;
+            .await?;
 
         if disable_tls_validation {
             warn!("Matrix Event Notifier client has TLS validation disabled");
         };
 
-        // build self
         let slf = Self {
             user_id,
             room_id,
-            access_token,
             user_password,
             device_id,
+
             client,
+            access_token: Mutex::new(access_token),
+            refresh_token: Mutex::new(None),
         };
 
         // try login and check access and config
         let mut retry = 0;
-        while retry < 3 {
+        let limit = 3;
+        while retry < limit {
             match slf.login().await {
                 Ok(_) => break,
                 Err(err) => {
-                    if retry < 3 {
+                    if retry < limit {
                         warn!("{:?} - retry in 5 seconds", err);
                         retry += 1;
                         time::sleep(Duration::from_secs(5)).await;
@@ -98,15 +94,6 @@ impl NotifierMatrix {
         slf.logged_in_check_sync().await?;
         slf.check_room_config().await?;
 
-        let mut rx = slf.client.subscribe_to_session_changes();
-        tokio::spawn(async move {
-            while let Ok(state_change) = rx.recv().await {
-                info!("Matrix Session State change: {:?}", state_change);
-                // TODO do we need to monitor these and re-login manually, or does the client do it already?
-            }
-        });
-
-        // all good
         Ok(slf)
     }
 
@@ -116,38 +103,57 @@ impl NotifierMatrix {
             return Ok(());
         }
 
-        if let Some(_session) = self.client.session() {
+        let refresh_token = self.refresh_token.lock().await.clone();
+        if let Some(token) = refresh_token {
+            let res = self
+                .client
+                .send_request(ruma::api::client::session::refresh_token::v3::Request::new(
+                    token,
+                ))
+                .await?;
+
+            {
+                let mut lock = self.access_token.lock().await;
+                *lock = Some(res.access_token);
+            }
+            {
+                let mut lock = self.refresh_token.lock().await;
+                *lock = res.refresh_token;
+            }
+
             if self.logged_in_check_sync().await.is_ok() {
                 return Ok(());
             }
-        } else if let Some(access_token) = &self.access_token {
-            let session = AuthSession::Matrix(MatrixSession {
-                meta: SessionMeta {
-                    user_id: self.user_id.clone(),
-                    device_id: self.device_id.clone(),
-                },
-                tokens: MatrixSessionTokens {
-                    access_token: access_token.clone(),
-                    refresh_token: None,
-                },
-            });
+        }
 
-            if self.client.restore_session(session).await.is_ok()
-                && self.logged_in_check_sync().await.is_ok()
-            {
-                return Ok(());
-            }
-        } else if let Some(password) = &self.user_password {
-            if self
-                .client
-                .matrix_auth()
-                .login_username(self.user_id.clone(), password)
-                .await
-                .is_ok()
-                && self.logged_in_check_sync().await.is_ok()
-            {
-                return Ok(());
-            }
+        if self.user_password.is_none() {
+            return Err(ErrorResponse::new(
+                ErrorResponseType::Connection,
+                "Cannot connect to Matrix with auth token and EVENT_MATRIX_USER_PASSWORD is empty",
+            ));
+        }
+
+        let session = self
+            .client
+            .log_in(
+                self.user_id.as_str(),
+                self.user_password.as_deref().unwrap_or_default(),
+                Some(&self.device_id),
+                Some("Rauthy Events"),
+            )
+            .await?;
+
+        {
+            let mut lock = self.access_token.lock().await;
+            *lock = Some(session.access_token);
+        }
+        {
+            let mut lock = self.refresh_token.lock().await;
+            *lock = session.refresh_token;
+        }
+
+        if self.logged_in_check_sync().await.is_ok() {
+            return Ok(());
         }
 
         Err(ErrorResponse::new(
@@ -157,37 +163,57 @@ impl NotifierMatrix {
     }
 
     async fn logged_in_check_sync(&self) -> Result<(), ErrorResponse> {
-        if self.client.logged_in() && self.client.sync_once(SyncSettings::default()).await.is_ok() {
-            info!("Successfully logged in to matrix");
-            Ok(())
-        } else {
-            Err(ErrorResponse::new(
+        let resp = self
+            .client
+            .send_request(ruma::api::client::account::whoami::v3::Request::new())
+            .await
+            .map_err(|err| {
+                ErrorResponse::new(
+                    ErrorResponseType::Connection,
+                    format!("Matrix Notifications Client is not logged in: {:?}", err),
+                )
+            })?;
+
+        if resp.user_id != self.user_id {
+            return Err(ErrorResponse::new(
                 ErrorResponseType::Connection,
                 "Matrix Notifications Client is not logged in".to_string(),
-            ))
-        }
+            ));
+        };
+
+        debug!("logged in to Matrix");
+        Ok(())
     }
 
     async fn check_room_config(&self) -> Result<(), ErrorResponse> {
-        if let Some(room) = self.client.get_room(&self.room_id) {
+        let resp = self
+            .client
+            .send_request(ruma::api::client::membership::joined_rooms::v3::Request::new())
+            .await?;
+
+        debug!(
+            "Looking for Matrix room id {} in joined rooms: {:?}",
+            self.room_id, resp.joined_rooms
+        );
+        let room = resp.joined_rooms.into_iter().find(|id| id == &self.room_id);
+
+        if let Some(room) = room {
+            assert_eq!(room, self.room_id);
             debug!("Matrix Notification Room found");
-
-            if room.state() == RoomState::Joined {
-                return Ok(());
-            }
-
+        } else {
             warn!("Not joined to the Matrix Notification Room. Trying to join now");
-            if room.join().await.is_ok() {
-                return Ok(());
-            } else {
-                panic!("Could not join the Matrix Notification Room");
-            }
+            let resp = self
+                .client
+                .send_request(
+                    ruma::api::client::membership::join_room_by_id::v3::Request::new(
+                        self.room_id.clone(),
+                    ),
+                )
+                .await?;
+            assert_eq!(resp.room_id, self.room_id);
         }
 
-        Err(ErrorResponse::new(
-            ErrorResponseType::Connection,
-            "Matrix Notifications Room could not be found - check EVENT_MATRIX_ROOM_ID".to_string(),
-        ))
+        Ok(())
     }
 }
 
@@ -196,7 +222,7 @@ impl Notify for NotifierMatrix {
     async fn notify(&self, notification: &Notification) -> Result<(), ErrorResponse> {
         debug!("Sending message to Matrix");
 
-        let build_msg = || {
+        let send_msg = || {
             let msg = if let Some(row_2) = &notification.row_2 {
                 format!(
                     "**{}**\n{}\n{}",
@@ -205,40 +231,89 @@ impl Notify for NotifierMatrix {
             } else {
                 format!("**{}**\n{}", notification.head, notification.row_1)
             };
-            RoomMessageEventContent::text_markdown(msg.clone())
+
+            self.client.send_request(
+                ruma::api::client::message::send_message_event::v3::Request::new(
+                    self.room_id.clone(),
+                    TransactionId::new(),
+                    &RoomMessageEventContent::text_markdown(msg),
+                )
+                .expect("Matrix message to always build fine"),
+            )
         };
 
-        if let Some(room) = self.client.get_room(&self.room_id) {
-            if let Err(err) = room.send(build_msg()).await {
-                error!("Sending Matrix Notification: {:?} - retrying once", err);
+        match send_msg().await {
+            Ok(res) => {
+                debug!("Successfully pushed matrix event with id {}", res.event_id);
+                Ok(())
+            }
+            Err(err) => {
+                error!("Error pushing matrix event: {:?}", err);
 
-                tokio::time::sleep(Duration::from_secs(1)).await;
+                let retries = 3;
+                for i in 1..=3 {
+                    match self.login().await {
+                        Ok(_) => {
+                            info!("Logged in to Matrix again");
+                            break;
+                        }
+                        Err(err) => {
+                            if i == retries {
+                                return Err(ErrorResponse::new(
+                                    ErrorResponseType::Connection,
+                                    format!("Unable to log in to Matrix after {} retries", retries),
+                                ));
+                            }
 
-                if self.logged_in_check_sync().await.is_err() {
-                    error!(
-                        "Matrix Notification Client is not logged in anymore when it was.\
-                        This should never happen."
-                    );
-                    if let Err(err) = self.logged_in_check_sync().await {
-                        error!("Unable to log back in again to Matrix: {:?}", err)
-                    } else {
-                        let _ = self.logged_in_check_sync().await;
-                        room.send(build_msg()).await.map_err(|err| {
-                            ErrorResponse::new(
-                                ErrorResponseType::Connection,
-                                format!("Matrix connection error: {:?}", err),
-                            )
-                        })?;
+                            error!("Error logging in to Matrix on {}. attempt: {:?}\nRetrying in 3 seconds", i, err);
+                            time::sleep(Duration::from_secs(3)).await;
+                            continue;
+                        }
                     }
                 }
-            }
 
-            Ok(())
-        } else {
-            Err(ErrorResponse::new(
-                ErrorResponseType::Connection,
-                "Matrix Notification Room went away - check your config".to_string(),
-            ))
+                match send_msg().await {
+                    Ok(_) => Ok(()),
+                    Err(err) => Err(ErrorResponse::new(
+                        ErrorResponseType::Connection,
+                        format!("Unable to post message to matrix after re-login: {}", err),
+                    )),
+                }
+            }
         }
+
+        //
+        // if let Some(room) = self.client.get_room(&self.room_id) {
+        //     if let Err(err) = room.send(build_msg()).await {
+        //         error!("Sending Matrix Notification: {:?} - retrying once", err);
+        //
+        //         tokio::time::sleep(Duration::from_secs(1)).await;
+        //
+        //         if self.logged_in_check_sync().await.is_err() {
+        //             error!(
+        //                 "Matrix Notification Client is not logged in anymore when it was.\
+        //                 This should never happen."
+        //             );
+        //             if let Err(err) = self.logged_in_check_sync().await {
+        //                 error!("Unable to log back in again to Matrix: {:?}", err)
+        //             } else {
+        //                 let _ = self.logged_in_check_sync().await;
+        //                 room.send(build_msg()).await.map_err(|err| {
+        //                     ErrorResponse::new(
+        //                         ErrorResponseType::Connection,
+        //                         format!("Matrix connection error: {:?}", err),
+        //                     )
+        //                 })?;
+        //             }
+        //         }
+        //     }
+        //
+        //     Ok(())
+        // } else {
+        //     Err(ErrorResponse::new(
+        //         ErrorResponseType::Connection,
+        //         "Matrix Notification Room went away - check your config".to_string(),
+        //     ))
+        // }
     }
 }
