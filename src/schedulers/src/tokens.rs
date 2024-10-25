84fefd286f6ed6acaf6a2027aabb328d3eb2718b
sebadob <sebastiandobe@mailbox.org>
2024-10-25 10:18:42 +0200

    migration of `schedulers::refresh_tokens_cleanup()`

--- src/schedulers/src/tokens.rs
+++ src/schedulers/src/tokens.rs
@@ -1,10 +1,11 @@
+use chrono::Utc;
+use hiqlite::{params, Param};
+use rauthy_common::is_hiqlite;
 use rauthy_models::app_state::DbPool;
 use rauthy_models::hiqlite::DB;
 use std::time::Duration;
-use time::OffsetDateTime;
 use tracing::{debug, error};
 
-// Cleans up old / expired / already used Refresh Tokens
 pub async fn refresh_tokens_cleanup(db: DbPool) {
     let mut interval = tokio::time::interval(Duration::from_secs(3600 * 3));
 
@@ -18,15 +19,22 @@ pub async fn refresh_tokens_cleanup(db: DbPool) {
 
         debug!("Running refresh_tokens_cleanup scheduler");
 
-        let now = OffsetDateTime::now_utc().unix_timestamp();
-        let res = sqlx::query("delete from refresh_tokens where exp < $1")
+        let now = Utc::now().timestamp();
+
+        if is_hiqlite() {
+            if let Err(err) = DB::client()
+                .execute("DELETE FROM refresh_tokens WHERE exp < $1", params!(now))
+                .await
+            {
+                error!("Refresh Token Cleanup Error: {:?}", err)
+            }
+        }
+        if let Err(err) = sqlx::query("DELETE FROM refresh_tokens WHERE exp < $1")
             .bind(now)
             .execute(&db)
-            .await;
-
-        match res {
-            Ok(_) => {}
-            Err(err) => error!("Refresh Token Cleanup Error: {:?}", err),
+            .await
+        {
+            error!("Refresh Token Cleanup Error: {:?}", err)
         }
     }
 }
