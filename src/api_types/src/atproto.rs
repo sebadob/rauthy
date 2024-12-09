b2dfad52a463d3832fdab2caba666c98ae718b33
avdb13 <avdb@keemail.me>
2024-12-09 21:16:21 +0000

    declare ATProto callback request type

--- src/api_types/src/atproto.rs
+++ src/api_types/src/atproto.rs
@@ -1,4 +1,4 @@
-use rauthy_common::constants::RE_URI;
+use rauthy_common::constants::{RE_ALNUM, RE_URI};
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 use validator::Validate;
@@ -18,4 +18,23 @@ pub struct LoginRequest {
     /// Validation: `[a-zA-Z0-9,.:/_-&?=~#!$'()*+%]+$`
     #[validate(regex(path = "*RE_URI", code = "[a-zA-Z0-9,.:/_-&?=~#!$'()*+%]+$"))]
     pub pkce_challenge: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Validate, ToSchema)]
+pub struct CallbackRequest {
+    /// Validation: `[a-zA-Z0-9]`
+    #[validate(regex(path = "*RE_ALNUM", code = "[a-zA-Z0-9]"))]
+    pub state: String,
+    /// Validation: `[a-zA-Z0-9,.:/_-&?=~#!$'()*+%]+$`
+    #[validate(regex(path = "*RE_URI", code = "[a-zA-Z0-9,.:/_-&?=~#!$'()*+%]+$"))]
+    pub code: String,
+    /// Validation: `[a-zA-Z0-9]`
+    #[validate(regex(path = "*RE_ALNUM", code = "[a-zA-Z0-9]"))]
+    pub iss: Option<String>,
+    /// Validation: `[a-zA-Z0-9]`
+    #[validate(regex(path = "*RE_ALNUM", code = "[a-zA-Z0-9]"))]
+    pub xsrf_token: String,
+    /// Validation: `[a-zA-Z0-9,.:/_-&?=~#!$'()*+%]+$`
+    #[validate(regex(path = "*RE_URI", code = "[a-zA-Z0-9,.:/_-&?=~#!$'()*+%]+$"))]
+    pub pkce_verifier: String,
 }
