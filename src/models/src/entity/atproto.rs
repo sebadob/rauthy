1d8e435b898376aa018a4a3f5257a3db4d0c6bbe
avdb13 <avdb@keemail.me>
2024-12-09 20:59:26 +0000

    introduce ATProto callback trait

--- src/models/src/entity/atproto.rs
+++ src/models/src/entity/atproto.rs
@@ -1,11 +1,14 @@
+use actix_web::{cookie::Cookie, http::header::HeaderValue, web};
 use atrium_identity::{
     did::CommonDidResolver,
     handle::{AtprotoHandleResolver, DnsTxtResolver as DnsTxtResolverTrait},
 };
 use atrium_oauth_client::{DefaultHttpClient, OAuthClient};
 use hickory_resolver::{proto::rr::rdata::TXT, TokioAsyncResolver};
+use rauthy_api_types::atproto;
+use rauthy_error::ErrorResponse;
 
-use crate::database::DB;
+use crate::{app_state::AppState, database::DB};
 
 pub type AtprotoClient = OAuthClient<
     DB,
@@ -35,4 +38,11 @@ impl DnsTxtResolverTrait for DnsTxtResolver {
         let txt_lookup = self.resolver.txt_lookup(query).await?;
         Ok(txt_lookup.iter().map(TXT::to_string).collect())
     }
+}
+
+pub trait AtprotoCallback {
+    async fn login_start(
+        data: &web::Data<AppState>,
+        payload: atproto::LoginRequest,
+    ) -> Result<(Cookie<'_>, String, HeaderValue), ErrorResponse>;
 }
