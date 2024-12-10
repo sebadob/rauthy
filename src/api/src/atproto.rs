6a1513e65d826cf0b2b1281e67f81d40a4cad7a6
avdb13 <avdb@keemail.me>
2024-12-10 09:47:39 +0000

    add ATProto callback handler

--- src/api/src/atproto.rs
+++ src/api/src/atproto.rs
@@ -2,17 +2,17 @@ use actix_web::{
     get,
     http::header::{self, HeaderValue, LOCATION},
     post,
-    web::{self, Json},
-    HttpResponse,
+    web::{self, Json, Query},
+    HttpRequest, HttpResponse,
 };
-use rauthy_api_types::atproto;
-use rauthy_error::ErrorResponse;
+use rauthy_api_types::{atproto, auth_providers::ProviderLookupResponse};
+use rauthy_error::{ErrorResponse, ErrorResponseType};
 use rauthy_models::{
     app_state::AppState,
     entity::{atproto::AtprotoCallback, auth_providers::AuthProviderCallback},
 };
 
-use crate::ReqPrincipal;
+use crate::{map_auth_step, ReqPrincipal};
 
 #[utoipa::path(
     get,
@@ -58,4 +58,48 @@ pub async fn post_login(
         .insert_header((LOCATION, location))
         .cookie(cookie)
         .body(xsrf_token))
+}
+
+#[utoipa::path(
+    post,
+    path = "/atproto/callback",
+    tag = "atproto",
+    responses(
+        (status = 200, description = "OK", body = ProviderLookupResponse),
+        (status = 400, description = "BadRequest", body = ErrorResponse),
+        (status = 404, description = "NotFound", body = ErrorResponse),
+    ),
+)]
+#[post("/atproto/callback")]
+#[tracing::instrument(
+    name = "post_provider_callback",
+    skip_all, fields(callback_id = payload.state)
+)]
+pub async fn post_callback(
+    data: web::Data<AppState>,
+    req: HttpRequest,
+    payload: Query<atproto::CallbackRequest>,
+    principal: ReqPrincipal,
+) -> Result<HttpResponse, ErrorResponse> {
+    principal.validate_session_auth_or_init()?;
+
+    let payload = payload.into_inner();
+    let session = principal.get_session()?;
+    let (auth_step, cookie) = <AuthProviderCallback as AtprotoCallback>::login_finish(
+        &data,
+        &req,
+        &payload,
+        session.clone(),
+    )
+    .await?;
+
+    let mut resp = map_auth_step(auth_step, &req).await?;
+    resp.add_cookie(&cookie).map_err(|err| {
+        ErrorResponse::new(
+            ErrorResponseType::Internal,
+            format!("Error adding cookie after map_auth_step: {}", err),
+        )
+    })?;
+
+    Ok(resp)
 }
