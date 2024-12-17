7b26c4f8c0f6f1b375dec7cd8484b69f668d0a4a
avdb13 <avdb@keemail.me>
2024-12-17 18:50:55 +0000

    separate atproto cache from native caches

--- src/models/src/database.rs
+++ src/models/src/database.rs
@@ -36,6 +36,7 @@ struct Migrations;
 pub enum Cache {
     App,
     AuthCode,
+    Atproto,
     DeviceCode,
     AuthProviderCallback,
     ClientDynamic,
@@ -243,13 +244,13 @@ impl Store<String, InternalStateData> for DB {
     type Error = hiqlite::Error;
 
     async fn get(&self, key: &String) -> Result<Option<InternalStateData>, Self::Error> {
-        Self::client().get(Cache::AuthProviderCallback, key).await
+        Self::client().get(Cache::Atproto, key).await
     }
 
     async fn set(&self, key: String, value: InternalStateData) -> Result<(), Self::Error> {
         Self::client()
             .put(
-                Cache::AuthProviderCallback,
+                Cache::Atproto,
                 key,
                 &value,
                 CACHE_TTL_AUTH_PROVIDER_CALLBACK,
@@ -258,15 +259,11 @@ impl Store<String, InternalStateData> for DB {
     }
 
     async fn del(&self, key: &String) -> Result<(), Self::Error> {
-        Self::client()
-            .delete(Cache::AuthProviderCallback, key.to_string())
-            .await
+        Self::client().delete(Cache::Atproto, key.to_string()).await
     }
 
     async fn clear(&self) -> Result<(), Self::Error> {
-        Self::client()
-            .clear_cache(Cache::AuthProviderCallback)
-            .await
+        Self::client().clear_cache(Cache::Atproto).await
     }
 }
 
@@ -275,22 +272,22 @@ impl StateStore for DB {}
 impl Store<Did, Session> for DB {
     type Error = hiqlite::Error;
 
-    async fn get(&self, key: &Did) -> Result<Option<Session>, Self::Error> {
-        Self::client().get(Cache::Session, key.to_string()).await
+    async fn get(&self, key: &Did) -> Result<Option<Atproto>, Self::Error> {
+        Self::client().get(Cache::Atproto, key.to_string()).await
     }
 
-    async fn set(&self, key: Did, value: Session) -> Result<(), Self::Error> {
+    async fn set(&self, key: Did, value: Atproto) -> Result<(), Self::Error> {
         Self::client()
-            .put(Cache::Session, key.to_string(), &value, CACHE_TTL_SESSION)
+            .put(Cache::Atproto, key.to_string(), &value, CACHE_TTL_SESSION)
             .await
     }
 
     async fn del(&self, key: &Did) -> Result<(), Self::Error> {
-        Self::client().delete(Cache::Session, key.to_string()).await
+        Self::client().delete(Cache::Atproto, key.to_string()).await
     }
 
     async fn clear(&self) -> Result<(), Self::Error> {
-        Self::client().clear_cache(Cache::Session).await
+        Self::client().clear_cache(Cache::Atproto).await
     }
 }
 
