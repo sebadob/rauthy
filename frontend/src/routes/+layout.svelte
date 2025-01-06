8e6c2494c0a42318b9018955883713ace3e94523
sebadob <sebastiandobe@mailbox.org>
2025-01-06 14:28:20 +0100

    add a global, cacheable and pre-compressed css

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import {onMount, type Snippet} from 'svelte'
+
+    import "../css/global.css";
+
+    let {
+        children,
+    }: {
+        children: Snippet,
+    } = $props();
+
+    let cookiesEnabled = $state(true);
+
+    onMount(() => {
+        cookiesEnabled = navigator.cookieEnabled;
+    });
+
+
+</script>
+
+<noscript>
+    <h2>You need to enable JavaScript</h2>
+</noscript>
+
+{#if cookiesEnabled}
+    {@render children()}
+{:else}
+    <div>
+        <h1>Cookies disabled</h1>
+        <p>
+            You need to enable Cookies.<br>
+            Without them, a safe interaction with Rauthy cannot happen.
+        </p>
+    </div>
+{/if}
+
