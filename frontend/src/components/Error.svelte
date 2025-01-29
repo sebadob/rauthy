c03503b43d4ede5e78b0f05a8b1aa52c8418fd4b
sebadob <sebastiandobe@mailbox.org>
2025-01-29 11:44:38 +0100

    rework `/error`

--- frontend/src/components/Error.svelte
+++ frontend/src/components/Error.svelte
@@ -1,17 +1,15 @@
-<script>
-    import {slide} from 'svelte/transition';
-    import IconChevronRight from "$lib/icons/IconChevronRight.svelte";
+<script lang="ts">
     import LangSelector from "$lib5/LangSelector.svelte";
     import Main from "$lib5/Main.svelte";
     import ContentCenter from "$lib5/ContentCenter.svelte";
     import {useI18n} from "$state/i18n.svelte";
     import Template from "$lib5/Template.svelte";
-    import {TPL_ERROR_TEXT, TPL_STATUS_CODE} from "../utils/constants.js";
+    import {TPL_ERROR_TEXT, TPL_STATUS_CODE} from "$utils/constants";
     import ThemeSwitch from "$lib5/ThemeSwitch.svelte";
+    import Expandable from "$lib5/Expandable.svelte";
 
     let t = useI18n();
 
-    let showDetails = $state(false);
     let statusCode = $state('');
     let errorText = $state('');
     let errorDetails = $state('');
@@ -29,31 +27,14 @@
             <p>{errorText}</p>
 
             {#if errorDetails}
-                <div
-                        role="button"
-                        tabindex="0"
-                        class="showDetails"
-                        onclick={() => showDetails = !showDetails}
-                        onkeypress={() => showDetails = !showDetails}
-                >
-                    {t.error.details}
-                    <div
-                            class="chevron"
-                            style:margin-top={showDetails ? '' : '-5px'}
-                            style:transform={showDetails ? 'rotate(90deg)' : 'rotate(180deg)'}
-                    >
-                        <IconChevronRight
-                                color="var(--col-act2)"
-                                width={16}
-                        />
-                    </div>
-                </div>
-
-                {#if showDetails}
-                    <div transition:slide class="details">
+                <Expandable>
+                    {#snippet summary()}
+                        {t.error.details}
+                    {/snippet}
+                    {#snippet details()}
                         {errorDetails}
-                    </div>
-                {/if}
+                    {/snippet}
+                </Expandable>
             {/if}
 
             <ThemeSwitch absolute/>
@@ -61,21 +42,3 @@
         </div>
     </ContentCenter>
 </Main>
-
-<style>
-    .chevron {
-        transition: all 250ms;
-    }
-
-    .showDetails {
-        display: inline-flex;
-        align-items: center;
-        gap: .1rem;
-        cursor: pointer;
-        color: var(--col-act2);
-    }
-
-    .showDetails:hover {
-        color: var(--col-act2a);
-    }
-</style>
