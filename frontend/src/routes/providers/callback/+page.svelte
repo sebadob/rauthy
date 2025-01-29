f7d6d02f6d1ee8314f71952243d48f969d979e9e
sebadob <sebastiandobe@mailbox.org>
2025-01-29 12:47:04 +0100

    rework `/providers/callback`

--- frontend/src/routes/providers/callback/+page.svelte
+++ frontend/src/routes/providers/callback/+page.svelte
@@ -1,23 +1,28 @@
-<script>
+<script lang="ts">
     import {
         getProviderToken,
         getVerifierUpstreamFromStorage,
-    } from "../../../utils/helpers";
-    import {postProviderCallback} from "../../../utils/dataFetching.js";
+    } from "$utils/helpers";
     import {onMount} from "svelte";
-    import WebauthnRequest from "../../../components/webauthn/WebauthnRequest.svelte";
+    import WebauthnRequest from "$lib5/WebauthnRequest.svelte";
     import BrowserCheck from "../../../components/BrowserCheck.svelte";
     import LangSelector from "$lib5/LangSelector.svelte";
-    import Button from "$lib/Button.svelte";
+    import Button from "$lib5/Button.svelte";
     import {useI18n} from "$state/i18n.svelte";
     import {useParam} from "$state/param.svelte";
     import ThemeSwitch from "$lib5/ThemeSwitch.svelte";
+    import type {MfaPurpose, WebauthnAdditionalData} from "$webauthn/types.ts";
+    import {fetchPost} from "$api/fetch.ts";
+    import type {WebauthnLoginResponse} from "$api/types/authorize.ts";
+    import {useIsDev} from "$state/is_dev.svelte.ts";
+    import type {ProviderCallbackRequest} from "$api/types/auth_provider.ts";
 
-    // will contain the same translations as /oidc/authorize
     let t = useI18n();
     let clientMfaForce = $state(false);
     let error = $state('');
-    let webauthnData = $state();
+
+    let userId: undefined | string = $state();
+    let mfaPurpose: undefined | MfaPurpose = $state();
 
     onMount(async () => {
         let pErr = useParam('error').get();
@@ -28,13 +33,29 @@
             return;
         }
 
-        let data = {
-            state: useParam('state').get(),
-            code: useParam('code').get(),
+        let state = useParam('state').get();
+        if (!state) {
+            error = "'state' is missing in URL"
+            return;
+        }
+        let code = useParam('code').get();
+        if (!code) {
+            error = "'code' is missing in URL"
+            return;
+        }
+
+        let payload: ProviderCallbackRequest = {
+            state,
+            code,
             pkce_verifier: getVerifierUpstreamFromStorage(),
             xsrf_token: getProviderToken(),
         };
-        let res = await postProviderCallback(data);
+
+        let url = '/auth/v1/providers/callback';
+        if (useIsDev().get()) {
+            url = '/auth/v1/dev/providers_callback';
+        }
+        let res = await fetchPost<undefined | WebauthnLoginResponse>(url, payload);
 
         if (res.status === 202) {
             // -> all good
@@ -42,34 +63,39 @@
         } else if (res.status === 200) {
             // -> all good, but needs additional passkey validation
             error = '';
-            webauthnData = await res.json();
+            let body = res.body;
+            if (body && 'user_id' in body && 'code' in body) {
+                // TODO is there a nicer way of making TS happy with type checking?
+                userId = body.user_id as string;
+                mfaPurpose = {Login: body.code as string};
+            } else {
+                console.error('did not receive a proper WebauthnLoginResponse after HTTP200');
+            }
         } else if (res.status === 204) {
             // in case of a 204, we have done a user federation on an existing account -> just redirect
             window.location.replace('/auth/v1/account');
         } else if (res.status === 403) {
             // we will get a forbidden if for instance the user already exists but without
             // any upstream provider link (or the wrong one)
-            let body = await res.json();
-            console.error(body);
-            error = body.message;
+            error = res.error?.message || 'HTTP 403 Forbidden';
         } else if (res.status === 406) {
             // 406 -> client forces MFA while the user has none
             error = t.authorize.clientForceMfa;
             clientMfaForce = true;
         } else {
-            let body = await res.text();
-            error = `HTTP ${res.status}: ${body}`;
+            error = `HTTP ${res.status}: ${res.error?.message}`;
         }
     });
 
-    function onWebauthnError(err) {
-        error = err || 'ERROR';
-        webauthnData = undefined;
+    function onWebauthnError(err: string) {
+        error = err;
+        mfaPurpose = undefined;
     }
 
-    function onWebauthnSuccess(res) {
-        if (res) {
-            window.location.replace(res.loc);
+    function onWebauthnSuccess(data?: WebauthnAdditionalData) {
+        console.log('onWebauthnSuccess', data);
+        if (data && 'loc' in data) {
+            window.location.replace(data.loc as string);
         }
     }
 
@@ -80,20 +106,21 @@
 </svelte:head>
 
 <BrowserCheck>
-    {#if webauthnData}
+    {#if mfaPurpose && userId}
         <WebauthnRequest
-                bind:data={webauthnData}
+                {userId}
+                purpose={mfaPurpose}
                 onSuccess={onWebauthnSuccess}
                 onError={onWebauthnError}
         />
     {:else if clientMfaForce}
         <div class="btn flex-col">
-            <Button on:click={() => window.location.href = '/auth/v1/account'}>
-                ACCOUNT LOGIN
+            <Button onclick={() => window.location.href = '/auth/v1/account'}>
+                Account
             </Button>
         </div>
     {:else if error}
-        <div class="error">
+        <div class="err">
             {error}
         </div>
     {/if}
@@ -106,10 +133,6 @@
     .btn {
         margin: 5px 0;
         display: flex;
-    }
-
-    .error {
-        color: var(--col-err)
     }
 
     .flex-col {
