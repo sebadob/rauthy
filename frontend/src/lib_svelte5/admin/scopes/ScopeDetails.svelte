ccfc74fb27b4e67a7b2fe3e1a4c440c7879fb350
sebadob <sebastiandobe@mailbox.org>
2025-02-17 17:23:20 +0100

    fix `<Tabs>` focussing on ID change

--- frontend/src/lib_svelte5/admin/scopes/ScopeDetails.svelte
+++ frontend/src/lib_svelte5/admin/scopes/ScopeDetails.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import ScopeConfig from "./ScopeConfig.svelte";
-    import ScopeDelete from "../../../components/admin/scopes/ScopeDelete.svelte";
+    import ScopeDelete from "./ScopeDelete.svelte";
     import type {UserAttrConfigValueResponse} from "$api/types/user_attrs.ts";
     import type {ScopeResponse} from "$api/types/scopes.ts";
     import {useI18n} from "$state/i18n.svelte.ts";
@@ -10,10 +10,12 @@
     let {
         attrs,
         scope,
+        scopes,
         onSave,
     }: {
         attrs: UserAttrConfigValueResponse[],
         scope: ScopeResponse,
+        scopes: ScopeResponse[],
         onSave: () => void,
     } = $props();
 
@@ -26,14 +28,24 @@
     ];
     let selected = $state(tabs[0]);
 
+    let focusFirst: undefined | (() => void) = $state();
+
+    $effect(() => {
+        if (scope.id) {
+            requestAnimationFrame(() => {
+                focusFirst?.();
+            });
+        }
+    });
+
 </script>
 
 <div class="flex">
-    <Tabs {tabs} bind:selected/>
+    <Tabs {tabs} bind:selected bind:focusFirst/>
 </div>
 
 {#if selected === ta.nav.config}
-    <ScopeConfig {attrs} {scope} {onSave}/>
+    <ScopeConfig {attrs} {scope} {scopes} {onSave}/>
 {:else if selected === t.common.delete}
     <ScopeDelete {scope} {onSave}/>
 {/if}
