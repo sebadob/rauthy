5380d926f3e9694350f604e9c23c1861a102f066
sebadob <sebastiandobe@mailbox.org>
2025-02-18 12:44:01 +0100

    cleanup

--- frontend/src/lib_svelte5/admin/groups/GroupDelete.svelte
+++ frontend/src/lib_svelte5/admin/groups/GroupDelete.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import Button from "$lib5/button/Button.svelte";
-    import {isDefaultScope} from "$utils/helpers.ts";
     import {useI18n} from "$state/i18n.svelte.ts";
     import {useI18nAdmin} from "$state/i18n_admin.svelte.ts";
     import {fetchDelete} from "$api/fetch.ts";
