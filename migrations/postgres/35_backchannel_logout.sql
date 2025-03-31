a4bdc0a06e2b46a11f042caeae4e50a2b49791c3
sebadob <sebastiandobe@mailbox.org>
2025-03-31 12:02:18 +0200

    add table `failed_backchannel_logouts` to db migrations

--- migrations/postgres/35_backchannel_logout.sql
+++ migrations/postgres/35_backchannel_logout.sql
@@ -3,3 +3,19 @@ ALTER TABLE clients
 
 CREATE INDEX backchannel_logout_uri_index
     ON clients (backchannel_logout_uri);
+
+CREATE TABLE failed_backchannel_logouts
+(
+    client_id   VARCHAR NOT NULL
+        CONSTRAINT failed_backchannel_logouts_clients_id_fk
+            REFERENCES clients
+            ON DELETE CASCADE,
+    sub         VARCHAR NOT NULL,
+    sid         VARCHAR NOT NULL,
+    retry_count INTEGER NOT NULL,
+    CONSTRAINT failed_backchannel_logouts_pk
+        PRIMARY KEY (client_id, sub, sid)
+);
+
+CREATE INDEX failed_backchannel_logouts_client_id_index
+    ON failed_backchannel_logouts (client_id);
