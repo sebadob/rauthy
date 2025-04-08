7b4b125e6c6f46b7983211206961eba3fd11d0ec
sebadob <sebastiandobe@mailbox.org>
2025-04-08 12:41:02 +0200

    migrate `ConfigEntity`

--- src/models/src/entity/config.rs
+++ src/models/src/entity/config.rs
@@ -1,7 +1,9 @@
 use serde::Deserialize;
 use sqlx::FromRow;
+use tokio_pg_mapper_derive::PostgresMapper;
 
-#[derive(Debug, Clone, FromRow, Deserialize)]
+#[derive(Debug, Clone, FromRow, Deserialize, PostgresMapper)]
+#[pg_mapper(table = "config")]
 pub struct ConfigEntity {
     pub id: String,
     pub data: Vec<u8>,
