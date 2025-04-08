b920f55c4a4097090cfef28ea968cf11e1099fc7
sebadob <sebastiandobe@mailbox.org>
2025-04-08 18:04:03 +0200

    migrate `UserLoginState`

--- src/models/src/entity/user_login_states.rs
+++ src/models/src/entity/user_login_states.rs
@@ -4,6 +4,7 @@ use hiqlite::{Param, params};
 use rauthy_common::is_hiqlite;
 use rauthy_error::ErrorResponse;
 use serde::Deserialize;
+use tokio_postgres::GenericClient;
 
 #[derive(Debug, Deserialize, sqlx::FromRow)]
 pub struct UserLoginState {
@@ -12,6 +13,17 @@ pub struct UserLoginState {
     pub user_id: String,
     pub client_id: String,
     pub session_id: Option<String>,
+}
+
+impl From<tokio_postgres::Row> for UserLoginState {
+    fn from(row: tokio_postgres::Row) -> Self {
+        Self {
+            timestamp: row.get("timestamp"),
+            user_id: row.get("user_id"),
+            client_id: row.get("client_id"),
+            session_id: row.get("session_id"),
+        }
+    }
 }
 
 /// CRUD
@@ -23,84 +35,40 @@ impl UserLoginState {
     ) -> Result<(), ErrorResponse> {
         let now = Utc::now().timestamp_millis();
 
-        if is_hiqlite() {
-            let mut txn = Vec::with_capacity(session_id.as_ref().map(|_| 2).unwrap_or(1));
-            txn.push((
-                r#"
+        let sql_1 = r#"
 INSERT INTO user_login_states(timestamp, user_id, client_id)
 SELECT $1, $2, $3
 WHERE NOT EXISTS (
     SELECT 1 FROM user_login_states
     WHERE user_id = $2 AND client_id = $3 AND session_id IS NULL
-)"#,
-                params!(now - 1, user_id.clone(), client_id.clone()),
-            ));
-            if session_id.is_some() {
-                txn.push((
-                    r#"
+)"#;
+        let sql_2 = r#"
 INSERT INTO user_login_states(timestamp, user_id, client_id, session_id)
 SELECT $1, $2, $3, $4
 WHERE NOT EXISTS (
     SELECT 1 FROM user_login_states
     WHERE user_id = $2 AND client_id = $3 AND session_id = $4
-)"#,
-                    params!(now, user_id, client_id, session_id),
-                ));
+)"#;
+
+        if is_hiqlite() {
+            let mut txn = Vec::with_capacity(session_id.as_ref().map(|_| 2).unwrap_or(1));
+            txn.push((sql_1, params!(now - 1, &user_id, &client_id)));
+            if session_id.is_some() {
+                txn.push((sql_2, params!(now, user_id, client_id, session_id)));
             }
 
             DB::hql().txn(txn).await?;
         } else {
-            let mut txn = DB::txn_sqlx().await?;
+            let mut cl = DB::pg().await?;
+            let txn = cl.transaction().await?;
 
-            // TODO for some reason, the query! returns an error with the prepared statement type
-            // for user_id, even though the query works just fine...
-            //             sqlx::query!(
-            //                 r#"
-            // INSERT INTO user_login_states(timestamp, user_id, client_id)
-            // SELECT $1, $2, $3
-            // WHERE NOT EXISTS (
-            //     SELECT 1 FROM user_login_states
-            //     WHERE user_id = $2 AND client_id = $3 AND session_id IS NULL
-            // )"#,
-            //                 now - 1,
-            //                 user_id,
-            //                 client_id,
-            //             )
-            //             .execute(&mut *txn)
-            //             .await?;
-
-            sqlx::query(
-                r#"
-INSERT INTO user_login_states(timestamp, user_id, client_id)
-SELECT $1, $2, $3
-WHERE NOT EXISTS (
-    SELECT 1 FROM user_login_states
-    WHERE user_id = $2 AND client_id = $3 AND session_id IS NULL
-)"#,
+            DB::pg_txn_append(&txn, sql_1, &[&(now - 1), &user_id, &client_id]).await?;
+            DB::pg_txn_append(
+                &txn,
+                sql_2,
+                &[&(now - 1), &user_id, &client_id, &session_id],
             )
-            .bind(now - 1)
-            .bind(user_id.clone())
-            .bind(client_id.clone())
-            .execute(&mut *txn)
             .await?;
-
-            if session_id.is_some() {
-                sqlx::query(
-                    r#"
-INSERT INTO user_login_states(timestamp, user_id, client_id, session_id)
-SELECT $1, $2, $3, $4
-WHERE NOT EXISTS (
-    SELECT 1 FROM user_login_states
-    WHERE user_id = $2 AND client_id = $3 AND session_id = $4
-)"#,
-                )
-                .bind(now)
-                .bind(user_id)
-                .bind(client_id)
-                .bind(session_id)
-                .execute(&mut *txn)
-                .await?;
-            }
 
             txn.commit().await?;
         }
@@ -109,20 +77,11 @@ WHERE NOT EXISTS (
     }
 
     pub async fn find_all_without_session() -> Result<Vec<Self>, ErrorResponse> {
+        let sql = "SELECT * FROM user_login_states WHERE session_id IS NULL";
         let res = if is_hiqlite() {
-            DB::hql()
-                .query_as(
-                    "SELECT * FROM user_login_states WHERE session_id IS NULL",
-                    params!(),
-                )
-                .await?
+            DB::hql().query_as(sql, params!()).await?
         } else {
-            sqlx::query_as!(
-                Self,
-                "SELECT * FROM user_login_states WHERE session_id IS NULL",
-            )
-            .fetch_all(DB::conn_sqlx())
-            .await?
+            DB::pg_query_map(sql, &[], 2).await?
         };
 
         Ok(res)
@@ -131,149 +90,90 @@ WHERE NOT EXISTS (
     pub async fn find_by_client_without_session(
         client_id: String,
     ) -> Result<Vec<Self>, ErrorResponse> {
+        let sql = "SELECT * FROM user_login_states WHERE client_id = $1 AND session_id IS NULL";
         let res = if is_hiqlite() {
-            DB::hql()
-                .query_as(
-                    "SELECT * FROM user_login_states WHERE client_id = $1 AND session_id IS NULL",
-                    params!(client_id),
-                )
-                .await?
+            DB::hql().query_as(sql, params!(client_id)).await?
         } else {
-            sqlx::query_as!(
-                Self,
-                "SELECT * FROM user_login_states WHERE client_id = $1 AND session_id IS NULL",
-                client_id
-            )
-            .fetch_all(DB::conn_sqlx())
-            .await?
+            DB::pg_query_map(sql, &[&client_id], 1).await?
         };
 
         Ok(res)
     }
 
     pub async fn find_by_user(user_id: String) -> Result<Vec<Self>, ErrorResponse> {
+        let sql = "SELECT * FROM user_login_states WHERE user_id = $1";
         let res = if is_hiqlite() {
-            DB::hql()
-                .query_as(
-                    "SELECT * FROM user_login_states WHERE user_id = $1",
-                    params!(user_id),
-                )
-                .await?
+            DB::hql().query_as(sql, params!(user_id)).await?
         } else {
-            sqlx::query_as!(
-                Self,
-                "SELECT * FROM user_login_states WHERE user_id = $1",
-                user_id
-            )
-            .fetch_all(DB::conn_sqlx())
-            .await?
+            DB::pg_query_map(sql, &[&user_id], 2).await?
         };
 
         Ok(res)
     }
 
     pub async fn find_by_session(session_id: String) -> Result<Vec<Self>, ErrorResponse> {
+        let sql = "SELECT * FROM user_login_states WHERE session_id = $1";
         let slf = if is_hiqlite() {
-            DB::hql()
-                .query_as(
-                    "SELECT * FROM user_login_states WHERE session_id = $1",
-                    params!(session_id),
-                )
-                .await?
+            DB::hql().query_as(sql, params!(session_id)).await?
         } else {
-            sqlx::query_as!(
-                Self,
-                "SELECT * FROM user_login_states WHERE session_id = $1",
-                session_id
-            )
-            .fetch_all(DB::conn_sqlx())
-            .await?
+            DB::pg_query_map(sql, &[&session_id], 2).await?
         };
 
         Ok(slf)
     }
 
     pub async fn delete(self) -> Result<(), ErrorResponse> {
+        let sql = "DELETE FROM user_login_states WHERE timestamp = $1 AND user_id = $2";
         if is_hiqlite() {
             DB::hql()
-                .execute(
-                    "DELETE FROM user_login_states WHERE timestamp = $1 AND user_id = $2",
-                    params!(self.timestamp, self.user_id),
-                )
+                .execute(sql, params!(self.timestamp, self.user_id))
                 .await?;
         } else {
-            sqlx::query!(
-                "DELETE FROM user_login_states WHERE timestamp = $1 AND user_id = $2",
-                self.timestamp,
-                self.user_id,
-            )
-            .execute(DB::conn_sqlx())
-            .await?;
+            DB::pg_execute(sql, &[&self.timestamp, &self.user_id]).await?;
         }
 
         Ok(())
     }
 
     pub async fn delete_all() -> Result<(), ErrorResponse> {
+        let sql = "DELETE FROM user_login_states";
         if is_hiqlite() {
-            DB::hql()
-                .execute("DELETE FROM user_login_states", params!())
-                .await?;
+            DB::hql().execute(sql, params!()).await?;
         } else {
-            sqlx::query!("DELETE FROM user_login_states")
-                .execute(DB::conn_sqlx())
-                .await?;
+            DB::pg_execute(sql, &[]).await?;
         }
 
         Ok(())
     }
 
     pub async fn delete_all_by_cid(cid: String) -> Result<(), ErrorResponse> {
+        let sql = "DELETE FROM user_login_states WHERE client_id = $1";
         if is_hiqlite() {
-            DB::hql()
-                .execute(
-                    "DELETE FROM user_login_states WHERE client_id = $1",
-                    params!(cid),
-                )
-                .await?;
+            DB::hql().execute(sql, params!(cid)).await?;
         } else {
-            sqlx::query!("DELETE FROM user_login_states WHERE client_id = $1", cid)
-                .execute(DB::conn_sqlx())
-                .await?;
+            DB::pg_execute(sql, &[&cid]).await?;
         }
 
         Ok(())
     }
 
     pub async fn delete_all_by_uid(uid: String) -> Result<(), ErrorResponse> {
+        let sql = "DELETE FROM user_login_states WHERE user_id = $1";
         if is_hiqlite() {
-            DB::hql()
-                .execute(
-                    "DELETE FROM user_login_states WHERE user_id = $1",
-                    params!(uid),
-                )
-                .await?;
+            DB::hql().execute(sql, params!(uid)).await?;
         } else {
-            sqlx::query!("DELETE FROM user_login_states WHERE user_id = $1", uid)
-                .execute(DB::conn_sqlx())
-                .await?;
+            DB::pg_execute(sql, &[&uid]).await?;
         }
 
         Ok(())
     }
 
     pub async fn delete_all_by_sid(sid: String) -> Result<(), ErrorResponse> {
+        let sql = "DELETE FROM user_login_states WHERE session_id = $1";
         if is_hiqlite() {
-            DB::hql()
-                .execute(
-                    "DELETE FROM user_login_states WHERE session_id = $1",
-                    params!(sid),
-                )
-                .await?;
+            DB::hql().execute(sql, params!(sid)).await?;
         } else {
-            sqlx::query!("DELETE FROM user_login_states WHERE session_id = $1", sid)
-                .execute(DB::conn_sqlx())
-                .await?;
+            DB::pg_execute(sql, &[&sid]).await?;
         }
 
         Ok(())
