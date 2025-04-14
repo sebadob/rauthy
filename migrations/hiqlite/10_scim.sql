798d63de074a6bd50512c332a1e1f99a09530e00
sebadob <sebastiandobe@mailbox.org>
2025-04-14 10:28:22 +0200

    add db migrations for `clients_scim`

--- migrations/hiqlite/10_scim.sql
+++ migrations/hiqlite/10_scim.sql
@@ -13,3 +13,19 @@ CREATE TABLE failed_scim_tasks
 
 CREATE INDEX failed_scim_tasks_client_id_index
     ON failed_scim_tasks (client_id);
+
+CREATE TABLE clients_scim
+(
+    client_id         TEXT NOT NULL
+        CONSTRAINT clients_scim_pk
+            PRIMARY KEY
+        CONSTRAINT clients_scim_clients_id_fk
+            REFERENCES clients
+            ON DELETE CASCADE ON UPDATE CASCADE,
+    bearer_token      BLOB NOT NULL,
+    base_endpoint     TEXT NOT NULL,
+    group_sync_prefix TEXT
+) STRICT;
+
+CREATE UNIQUE INDEX clients_scim_client_id_uindex
+    ON clients_scim (client_id);
--- migrations/postgres/V6__scim.sql
+++ migrations/postgres/V6__scim.sql
@@ -10,3 +10,20 @@ CREATE TABLE failed_scim_tasks
     CONSTRAINT failed_scim_tasks_pk
         PRIMARY KEY (action, client_id, resource)
 );
+
+
+CREATE TABLE clients_scim
+(
+    client_id         VARCHAR NOT NULL
+        CONSTRAINT clients_scim_pk
+            PRIMARY KEY
+        CONSTRAINT clients_scim_clients_id_fk
+            REFERENCES clients
+            ON DELETE CASCADE ON UPDATE CASCADE,
+    bearer_token      BYTEA   NOT NULL,
+    base_endpoint     VARCHAR NOT NULL,
+    group_sync_prefix VARCHAR
+);
+
+CREATE UNIQUE INDEX clients_scim_client_id_uindex
+    ON clients_scim (client_id);
