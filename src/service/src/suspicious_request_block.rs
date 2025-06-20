a98dd9acd3ca569f9b741d37df2362e9c4dd4217
sebadob <sebastiandobe@mailbox.org>
2025-06-20 21:35:09 +0200

    add more scan targets

--- src/service/src/suspicious_request_block.rs
+++ src/service/src/suspicious_request_block.rs
@@ -1,20 +1,25 @@
-const START_WITH_TARGETS: [&str; 10] = [
+const START_WITH_TARGETS: [&str; 14] = [
+    "/2018",
+    "/2019",
+    "/2020",
     "/../",
     "/.aws/",
     "/.env",
     "/.git/",
     "/.kube/",
     "/.ssh/",
+    "/.well-knownold/",
+    "/blog",
     "/docker-compose",
     "/etc/",
     "/http",
-    "/wp",
 ];
 
 const ENDS_WITH_TARGETS: [&str; 8] = [
     ".json", ".yaml", ".yml", ".php", ".sql", ".xml", ".tar.gz", ".zip",
 ];
 
+const CONTAINS_TARGETS: [&str; 1] = ["/wp-"];
 /// Scans the given url path for common scan targets from bots and attackers.
 pub fn is_scan_target(request_path: &str) -> bool {
     for target in START_WITH_TARGETS {
@@ -25,6 +30,12 @@ pub fn is_scan_target(request_path: &str) -> bool {
 
     for target in ENDS_WITH_TARGETS {
         if request_path.ends_with(target) {
+            return true;
+        }
+    }
+
+    for target in CONTAINS_TARGETS {
+        if request_path.contains(target) {
             return true;
         }
     }
