6a239711ce8d187566a870284929a263b3fdb8ce
sebadob <sebastiandobe@mailbox.org>
2025-07-17 16:58:07 +0200

    refactor temp migrations into real ones for hiqlite + postgres

--- migrations/hiqlite/18_pam.sql
+++ migrations/hiqlite/18_pam.sql
@@ -1,2 +1,106 @@
 ALTER TABLE users
     ADD allow_pam_logins INTEGER DEFAULT 0 NOT NULL;
+
+CREATE TABLE pam_groups
+(
+    id            INTEGER NOT NULL
+        CONSTRAINT pam_groups_pk
+            PRIMARY KEY AUTOINCREMENT,
+    name          TEXT    NOT NULL,
+    is_user_group INTEGER NOT NULL
+) STRICT;
+
+CREATE UNIQUE INDEX pam_groups_name_uindex
+    ON pam_groups (name);
+
+CREATE TABLE pam_hosts
+(
+    id        TEXT    NOT NULL
+        CONSTRAINT pam_hosts_pk
+            PRIMARY KEY,
+    hostname  TEXT    NOT NULL,
+    gid       INTEGER NOT NULL
+        CONSTRAINT pam_hosts_pam_groups_id_fk
+            REFERENCES pam_groups,
+    secret    BLOB    NOT NULL,
+    force_mfa INTEGER NOT NULL,
+    notes     TEXT
+) STRICT;
+
+CREATE INDEX pam_hosts_gid_index
+    ON pam_hosts (gid);
+
+CREATE UNIQUE INDEX pam_hosts_hostname_gid_uindex
+    ON pam_hosts (hostname, gid);
+
+CREATE TABLE pam_hosts_ips
+(
+    host_id TEXT NOT NULL
+        CONSTRAINT pam_hosts_ips_pam_hosts_id_fk
+            REFERENCES pam_hosts
+            ON DELETE CASCADE,
+    ip      TEXT NOT NULL,
+    CONSTRAINT pam_hosts_ips_pk
+        PRIMARY KEY (host_id, ip)
+) STRICT;
+
+CREATE INDEX pam_hosts_ips_ip_index
+    ON pam_hosts_ips (ip);
+
+CREATE TABLE pam_hosts_aliases
+(
+    host_id TEXT NOT NULL
+        CONSTRAINT pam_hosts_aliases_pam_hosts_id_fk
+            REFERENCES pam_hosts
+            ON DELETE CASCADE,
+    alias   TEXT NOT NULL,
+    CONSTRAINT pam_hosts_aliases_pk
+        PRIMARY KEY (host_id, alias)
+) STRICT;
+
+CREATE INDEX pam_hosts_aliases_alias_index
+    ON pam_hosts_aliases (alias);
+
+CREATE TABLE pam_users
+(
+    id    INTEGER NOT NULL
+        CONSTRAINT pam_users_pk
+            PRIMARY KEY AUTOINCREMENT,
+    name  TEXT    NOT NULL,
+    gid   INTEGER NOT NULL
+        CONSTRAINT pam_users_pam_groups_id_fk
+            REFERENCES pam_groups,
+    email TEXT    not null
+        CONSTRAINT pam_users_users_email_fk
+            REFERENCES users (email)
+            ON UPDATE CASCADE ON DELETE CASCADE,
+    shell TEXT    NOT NULL
+) STRICT;
+
+CREATE INDEX pam_users_gid_index
+    ON pam_users (gid);
+
+CREATE UNIQUE INDEX pam_users_name_uindex
+    ON pam_users (name);
+
+CREATE UNIQUE INDEX pam_users_email_uindex
+    ON pam_users (email);
+
+CREATE TABLE pam_rel_groups_users
+(
+    gid INTEGER NOT NULL
+        CONSTRAINT pam_rel_groups_users_pam_groups_id_fk
+            REFERENCES pam_groups
+            ON DELETE CASCADE,
+    uid INTEGER NOT NULL
+        CONSTRAINT pam_rel_groups_users_pam_users_id_fk
+            REFERENCES pam_users
+            ON DELETE CASCADE,
+    CONSTRAINT pam_rel_groups_users_pk
+        PRIMARY KEY (gid, uid)
+) STRICT;
+
+-- make sure uid + gid sequences start at 100_000
+INSERT INTO sqlite_sequence (name, seq)
+VALUES ('pam_groups', 100000),
+       ('pam_users', 100000);
--- migrations/postgres/V13__pam.sql
+++ migrations/postgres/V13__pam.sql
@@ -1,2 +1,104 @@
 ALTER TABLE users
     ADD allow_pam_logins BOOL DEFAULT false NOT NULL;
+
+CREATE TABLE pam_groups
+(
+    id            bigserial NOT NULL
+        CONSTRAINT pam_groups_pk
+            PRIMARY KEY,
+    name          varchar   NOT NULL ,
+    is_user_group bool      NOT NULL
+);
+
+create unique index pam_groups_name_uindex
+    on pam_groups (name);
+
+CREATE TABLE pam_hosts
+(
+    id        VARCHAR NOT NULL
+        CONSTRAINT pam_hosts_pk
+            PRIMARY KEY,
+    hostname  varchar NOT NULL ,
+    gid       bigint  NOT NULL
+        constraint pam_hosts_pam_groups_id_fk
+            references pam_groups,
+    secret    bytea   NOT NULL ,
+    force_mfa bool    NOT NULL ,
+    notes     varchar
+);
+
+CREATE INDEX pam_hosts_gid_index
+    ON pam_hosts (gid);
+
+CREATE UNIQUE INDEX pam_hosts_hostname_gid_uindex
+    ON pam_hosts (hostname, gid);
+
+CREATE TABLE pam_hosts_ips
+(
+    host_id varchar NOT NULL
+        CONSTRAINT pam_hosts_ips_pam_hosts_id_fk
+            REFERENCES pam_hosts
+            ON DELETE CASCADE ,
+    ip      varchar NOT NULL ,
+    CONSTRAINT pam_hosts_ips_pk
+        PRIMARY KEY (host_id, ip)
+);
+
+CREATE INDEX pam_hosts_ips_ip_index
+    ON pam_hosts_ips (ip);
+
+CREATE TABLE pam_hosts_aliases
+(
+    host_id varchar NOT NULL
+        CONSTRAINT pam_hosts_aliases_pam_hosts_id_fk
+            REFERENCES pam_hosts
+            ON DELETE CASCADE ,
+    alias   varchar NOT NULL,
+    CONSTRAINT pam_hosts_aliases_pk
+        PRIMARY KEY (host_id, alias)
+);
+
+CREATE INDEX pam_hosts_aliases_alias_index
+    ON pam_hosts_aliases (alias);
+
+CREATE TABLE pam_users
+(
+    id    bigserial
+        CONSTRAINT pam_users_pk
+            PRIMARY KEY,
+    name  varchar NOT NULL ,
+    gid   bigint  NOT NULL
+        CONSTRAINT pam_users_pam_groups_id_fk
+            REFERENCES pam_groups,
+    email varchar NOT NULL
+        CONSTRAINT pam_users_users_email_fk
+            REFERENCES users (email)
+            ON UPDATE CASCADE ON DELETE CASCADE ,
+    shell varchar NOT NULL
+);
+
+CREATE UNIQUE INDEX pam_users_email_uindex
+    ON pam_users (email);
+
+CREATE INDEX pam_users_gid_index
+    ON pam_users (gid);
+
+CREATE UNIQUE INDEX pam_users_name_uindex
+    ON pam_users (name);
+
+CREATE TABLE pam_rel_groups_users
+(
+    gid bigint NOT NULL
+        CONSTRAINT pam_rel_groups_users_pam_groups_id_fk
+            REFERENCES pam_groups
+            ON DELETE CASCADE ,
+    uid bigint NOT NULL
+        CONSTRAINT pam_rel_groups_users_pam_users_id_fk
+            REFERENCES pam_users
+            ON DELETE CASCADE ,
+    CONSTRAINT pam_rel_groups_users_pk
+        PRIMARY KEY (gid, uid)
+);
+
+ALTER SEQUENCE pam_groups_id_seq RESTART WITH 100000;
+ALTER SEQUENCE pam_users_id_seq RESTART WITH 100000;
