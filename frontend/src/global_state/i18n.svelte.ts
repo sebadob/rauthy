916b960133309706fc6048ac643d18001059a02c
Daniel Bunte <daniel.bunte.84@gmail.com>
2025-08-28 16:23:01 +0200

    feat(i18n): Adds translations for Norwegian Bokmål
    Replaces switch-case with Map-lookup.

--- frontend/src/global_state/i18n.svelte.ts
+++ frontend/src/global_state/i18n.svelte.ts
@@ -4,6 +4,7 @@ import {I18nEn} from "../i18n/common/en";
 import {I18nDe} from "../i18n/common/de";
 import {I18nKo} from "../i18n/common/ko";
 import {I18nZh} from "../i18n/common/zh";
+import {I18nNb} from "../i18n/common/nb";
 
 // This hack makes typescript happy and is fine as long as
 // we `initI18n()` as the very first thing in +layout
@@ -25,20 +26,15 @@ export function useI18n(): I18n {
     return _i18n;
 }
 
+const i18nMap: Record<string, I18n> = {
+    en: I18nEn,
+    de: I18nDe,
+    ko: I18nKo,
+    nb: I18nNb,
+    zh: I18nZh,
+};
+
 export function initI18n() {
     let lang = useLang();
-    switch (lang) {
-        case 'de':
-            _i18n = I18nDe;
-            break;
-        case 'ko':
-            _i18n = I18nKo;
-            break;
-        case 'zh':
-            _i18n = I18nZh;
-            break;
-        default:
-            _i18n = I18nEn;
-            break;
-    }
+    _i18n = i18nMap[lang] ?? I18nEn;
 }
