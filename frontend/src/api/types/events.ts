251374437627cb3b5b5edf7575cdee438df40e36
sebadob <sebastiandobe@mailbox.org>
2025-09-12 12:04:38 +0200

    update UI filter with new event types

--- frontend/src/api/types/events.ts
+++ frontend/src/api/types/events.ts
@@ -1,41 +1,43 @@
 export type EventLevel = 'info' | 'notice' | 'warning' | 'critical';
-export type EventType = 'InvalidLogins'
-    | 'IpBlacklisted'
-    | 'IpBlacklistRemoved'
-    | 'JwksRotated'
-    | 'NewUserRegistered'
-    | 'NewRauthyAdmin'
-    | 'NewRauthyVersion'
-    | 'PossibleBruteForce'
-    | 'RauthyStarted'
-    | 'RauthyHealthy'
-    | 'RauthyUnhealthy'
-    | 'SecretsMigrated'
-    | 'UserEmailChange'
-    | 'UserPasswordReset'
-    | 'Test'
-    | 'BackchannelLogoutFailed'
-    | 'ScimTaskFailed'
-    | 'ForcedLogout'
-    | 'UserLoginRevoke'
-    | 'SuspiciousApiScan'
-    ;
+export type EventType =
+	| 'InvalidLogins'
+	| 'IpBlacklisted'
+	| 'IpBlacklistRemoved'
+	| 'JwksRotated'
+	| 'NewUserRegistered'
+	| 'NewRauthyAdmin'
+	| 'NewRauthyVersion'
+	| 'PossibleBruteForce'
+	| 'RauthyStarted'
+	| 'RauthyHealthy'
+	| 'RauthyUnhealthy'
+	| 'SecretsMigrated'
+	| 'UserEmailChange'
+	| 'UserPasswordReset'
+	| 'Test'
+	| 'BackchannelLogoutFailed'
+	| 'ScimTaskFailed'
+	| 'ForcedLogout'
+	| 'UserLoginRevoke'
+	| 'LoginNewLocation'
+	| 'SuspiciousApiScan'
+	| 'TokenIssued';
 
 export interface EventsRequest {
-    /// Unix timestamp in seconds
-    from: number,
-    /// Unix timestamp in seconds
-    until?: number,
-    level: EventLevel,
-    typ?: EventType,
+	/// Unix timestamp in seconds
+	from: number;
+	/// Unix timestamp in seconds
+	until?: number;
+	level: EventLevel;
+	typ?: EventType;
 }
 
 export interface EventResponse {
-    id: string,
-    timestamp: number,
-    level: EventLevel,
-    typ: EventType,
-    ip?: string,
-    data?: number,
-    text?: string,
+	id: string;
+	timestamp: number;
+	level: EventLevel;
+	typ: EventType;
+	ip?: string;
+	data?: number;
+	text?: string;
 }
