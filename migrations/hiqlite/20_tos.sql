25f90ad1c81e8e66ccbf67ed028cc66354c0d5a4
sebadob <sebastiandobe@mailbox.org>
2025-10-16 10:08:06 +0200

    add `tos_user_accept` table to DB migrations

--- migrations/hiqlite/20_tos.sql
+++ migrations/hiqlite/20_tos.sql
@@ -10,3 +10,16 @@ CREATE TABLE tos
 
 CREATE UNIQUE INDEX tos_ts_uindex
     ON tos (ts DESC);
+
+CREATE TABLE tos_user_accept
+(
+    user_id  TEXT    NOT NULL,
+    tos_ts   INTEGER NOT NULL,
+    ts       INTEGER NOT NULL,
+    location TEXT    NOT NULL,
+    CONSTRAINT tos_user_accept_pk
+        PRIMARY KEY (user_id, tos_ts)
+) STRICT;
+
+CREATE UNIQUE INDEX tos_user_accept_user_id_tos_Ts_uindex
+    ON tos_user_accept (user_id, tos_Ts);
--- migrations/postgres/V15__tos.sql
+++ migrations/postgres/V15__tos.sql
@@ -10,3 +10,16 @@ CREATE TABLE tos
 
 CREATE UNIQUE INDEX tos_ts_uindex
     ON tos (ts DESC);
+
+CREATE TABLE tos_user_accept
+(
+    user_id  VARCHAR NOT NULL,
+    tos_ts   BIGINT  NOT NULL,
+    ts       BIGINT  NOT NULL,
+    location VARCHAR NOT NULL,
+    CONSTRAINT tos_user_accept_pk
+        PRIMARY KEY (user_id, tos_ts)
+);
+
+CREATE UNIQUE INDEX tos_user_accept_user_id_tos_Ts_uindex
+    ON tos_user_accept (user_id, tos_Ts);
