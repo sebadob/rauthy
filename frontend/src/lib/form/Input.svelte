06a8806c70f0cccdb72a300403d08db8f83a2533
JuliDi <20155974+JuliDi@users.noreply.github.com>
2025-10-16 18:02:18 +0200

    Frontend improvements for Input, LabeledValue and Button

--- frontend/src/lib/form/Input.svelte
+++ frontend/src/lib/form/Input.svelte
@@ -1,204 +1,205 @@
 <script lang="ts">
-    import {slide} from "svelte/transition";
-    import {useI18n} from "$state/i18n.svelte";
-    import type {FullAutoFill} from "svelte/elements";
-    import {genKey} from "$utils/helpers";
+	import { slide } from 'svelte/transition';
+	import { useI18n } from '$state/i18n.svelte';
+	import type { FullAutoFill } from 'svelte/elements';
+	import { genKey } from '$utils/helpers';
 
-    let {
-        ref = $bindable(),
-        typ = 'text',
-        id = genKey(),
-        name,
-        value = $bindable(),
-        label = '',
+	let {
+		ref = $bindable(),
+		typ = 'text',
+		id = genKey(),
+		name,
+		value = $bindable(),
+		label = '',
 
-        autocomplete = 'on',
-        placeholder = '',
-        disabled = false,
+		autocomplete = 'on',
+		placeholder = '',
+		disabled = false,
 
-        minLength,
-        maxLength,
-        min,
-        max,
-        step = 1,
+		minLength,
+		maxLength,
+		min,
+		max,
+		step = 1,
 
-        required = false,
-        pattern,
-        errMsg,
-        isError = $bindable(false),
+		required = false,
+		pattern,
+		errMsg,
+		isError = $bindable(false),
 
-        width = 'inherit',
+		width = 'inherit',
 
-        onBlur,
-        onEnter,
-        onInput,
-        onLeft,
-        onRight,
-        onUp,
-        onDown,
-        onSubmit,
-    }: {
-        ref?: undefined | HTMLInputElement,
-        typ?: string,
-        id?: string,
-        name?: string,
-        value?: string | number,
-        label?: string,
-        autocomplete?: FullAutoFill | null | undefined,
-        placeholder?: string,
-        disabled?: boolean | null | undefined,
-        minLength?: number | null | undefined,
-        maxLength?: number | null | undefined,
-        min?: string,
-        max?: string,
-        step?: number,
-        required?: boolean,
-        pattern?: string,
-        errMsg?: string,
-        isError?: boolean,
-        width?: string,
+		onBlur,
+		onEnter,
+		onInput,
+		onLeft,
+		onRight,
+		onUp,
+		onDown,
+		onSubmit
+	}: {
+		ref?: undefined | HTMLInputElement;
+		typ?: string;
+		id?: string;
+		name?: string;
+		value?: string | number;
+		label?: string;
+		autocomplete?: FullAutoFill | null | undefined;
+		placeholder?: string;
+		disabled?: boolean | null | undefined;
+		minLength?: number | null | undefined;
+		maxLength?: number | null | undefined;
+		min?: string;
+		max?: string;
+		step?: number;
+		required?: boolean;
+		pattern?: string;
+		errMsg?: string;
+		isError?: boolean;
+		width?: string;
 
-        onBlur?: () => void,
-        onEnter?: () => void,
-        onInput?: () => void,
-        onLeft?: () => void,
-        onRight?: () => void,
-        onUp?: () => void,
-        onDown?: () => void,
-        onSubmit?: (event: Event & { currentTarget: EventTarget & HTMLInputElement }) => void,
-    } = $props();
+		onBlur?: () => void;
+		onEnter?: () => void;
+		onInput?: () => void;
+		onLeft?: () => void;
+		onRight?: () => void;
+		onUp?: () => void;
+		onDown?: () => void;
+		onSubmit?: (event: Event & { currentTarget: EventTarget & HTMLInputElement }) => void;
+	} = $props();
 
-    let t = useI18n();
+	let t = useI18n();
 
-    $effect(() => {
-        // this unused assignment is used only to trigger a re-validation on any input change
-        let v = value;
-        isValid();
-    });
+	let touched = $state(false);
 
-    function onblur(event: FocusEvent & { currentTarget: EventTarget & HTMLInputElement }) {
-        isError = false;
-        // the animation frame triggers some screen readers to re-read errors if they still exist
-        requestAnimationFrame(() => {
-            isValid();
-            onBlur?.();
-        });
-    }
+	$effect(() => {
+		// this unused assignment is used only to trigger a re-validation on any input change
+		let v = value;
+		if (touched) {
+			isValid();
+		}
+	});
 
-    function oninput(event: Event & { currentTarget: EventTarget & HTMLInputElement }) {
-        onInput?.();
-    }
+	function onblur(event: FocusEvent & { currentTarget: EventTarget & HTMLInputElement }) {
+		touched = true;
+		isError = false;
+		// the animation frame triggers some screen readers to re-read errors if they still exist
+		requestAnimationFrame(() => {
+			isValid();
+			onBlur?.();
+		});
+	}
 
-    function oninvalid(event: Event & { currentTarget: EventTarget & HTMLInputElement }) {
-        event.preventDefault();
-        isError = true;
-    }
+	function oninput(event: Event & { currentTarget: EventTarget & HTMLInputElement }) {
+		if (touched) {
+			isValid();
+		}
+		onInput?.();
+	}
 
-    function onkeydown(ev: KeyboardEvent) {
-        isValid();
-        switch (ev.code) {
-            case 'Enter':
-                if (isValid()) {
-                    onEnter?.();
-                }
-                break;
-            case 'ArrowUp':
-                onUp?.();
-                break;
-            case 'ArrowDown':
-                onDown?.();
-                break;
-            case 'ArrowLeft':
-                onLeft?.();
-                break;
-            case 'ArrowRight':
-                onRight?.();
-                break;
-        }
-    }
+	function oninvalid(event: Event & { currentTarget: EventTarget & HTMLInputElement }) {
+		event.preventDefault();
+		isError = true;
+	}
 
-    function isValid() {
-        let validity = ref?.validity;
-        if (validity) {
-            isError = !validity.valid;
-            return validity.valid;
-        }
-        isError = false;
-        return true;
-    }
+	function onkeydown(ev: KeyboardEvent) {
+		isValid();
+		switch (ev.code) {
+			case 'Enter':
+				if (isValid()) {
+					onEnter?.();
+				}
+				break;
+			case 'ArrowUp':
+				onUp?.();
+				break;
+			case 'ArrowDown':
+				onDown?.();
+				break;
+			case 'ArrowLeft':
+				onLeft?.();
+				break;
+			case 'ArrowRight':
+				onRight?.();
+				break;
+		}
+	}
 
+	function isValid() {
+		let validity = ref?.validity;
+		if (validity) {
+			isError = !validity.valid;
+			return validity.valid;
+		}
+		isError = false;
+		return true;
+	}
 </script>
 
-<div style:width={width}>
-    <input
-            bind:this={ref}
-            type={typ}
-            {id}
-            {name}
-            title={errMsg}
-            aria-label={label || placeholder}
-            bind:value
-
-            {autocomplete}
-            {placeholder}
-            aria-placeholder={placeholder}
-            {disabled}
-            aria-disabled={disabled}
-
-            minlength={minLength || undefined}
-            maxlength={maxLength || undefined}
-            min={min || undefined}
-            max={max || undefined}
-            {step}
-            required={required || undefined}
-            aria-required={required || false}
-            aria-invalid={isError}
-            pattern={pattern || undefined}
-
-            {oninput}
-            {oninvalid}
-            {onblur}
-            {onkeydown}
-            onsubmit={onSubmit}
-    />
-    <div aria-live="assertive" class="label">
-        <label for={id} class="font-label noselect" data-required={required}>
-            {label}
-        </label>
-        {#if isError}
-            <div
-                    aria-relevant="all"
-                    class="error"
-                    class:errWithLabel={!!label}
-                    transition:slide={{duration: 150}}
-            >
-                {errMsg || t.common.invalidInput}
-            </div>
-        {/if}
-    </div>
+<div style:width>
+	<div aria-live="assertive" class="label">
+		<label for={id} class="font-label noselect" data-required={required}>
+			{label}
+		</label>
+	</div>
+	<input
+		bind:this={ref}
+		type={typ}
+		{id}
+		{name}
+		title={errMsg}
+		aria-label={label || placeholder}
+		bind:value
+		{autocomplete}
+		{placeholder}
+		aria-placeholder={placeholder}
+		{disabled}
+		aria-disabled={disabled}
+		minlength={minLength || undefined}
+		maxlength={maxLength || undefined}
+		min={min || undefined}
+		max={max || undefined}
+		{step}
+		required={required || undefined}
+		aria-required={required || false}
+		aria-invalid={isError}
+		pattern={pattern || undefined}
+		class:invalid={isError}
+		{oninput}
+		{oninvalid}
+		{onblur}
+		{onkeydown}
+		onsubmit={onSubmit}
+	/>
+	{#if isError}
+		<div
+			aria-relevant="all"
+			class="error"
+			class:errWithLabel={!!label}
+			transition:slide={{ duration: 150 }}
+		>
+			{errMsg || t.common.invalidInput}
+		</div>
+	{/if}
 </div>
 
 <style>
-    label, .error {
-        line-height: 1.1rem;
-        font-size: .9rem;
-    }
+	label {
+		font-size: 0.9em;
+		flex-wrap: wrap;
+	}
 
-    label {
-        flex-wrap: wrap;
-    }
+	.label {
+		width: 100%;
+		margin-bottom: -0.3rem;
+		padding-left: 0.1rem;
+		padding-top: 0.6rem;
+	}
 
-    .label {
-        width: 100%;
-        margin-top: -1.1rem;
-        padding: .5rem;
-    }
-
-    .error {
-        color: hsl(var(--error));
-    }
-
-    .errWithLabel {
-        margin-top: -.5rem;
-    }
+	.error {
+		color: hsl(var(--error));
+		margin-top: -0.4rem;
+		padding-left: 0.3rem;
+		font-size: 0.8rem;
+	}
 </style>
--- frontend/src/routes/users/register/+page.svelte
+++ frontend/src/routes/users/register/+page.svelte
@@ -1,170 +1,218 @@
 <script lang="ts">
-    import Button from "$lib5/button/Button.svelte";
-    import {IS_DEV, TPL_RESTRICTED_EMAIL_DOMAIN} from "$utils/constants";
-    import Input from "$lib5/form/Input.svelte";
-    import LangSelector from "$lib5/LangSelector.svelte";
-    import Main from "$lib5/Main.svelte";
-    import ContentCenter from "$lib5/ContentCenter.svelte";
-    import {useI18n} from "$state/i18n.svelte";
-    import Template from "$lib5/Template.svelte";
-    import {useParam} from "$state/param.svelte";
-    import ThemeSwitch from "$lib5/ThemeSwitch.svelte";
-    import Form from "$lib5/form/Form.svelte";
-    import {PATTERN_USER_NAME} from "$utils/patterns";
-    import type {NewUserRegistrationRequest} from "$api/types/register.ts";
-    import {fetchPost} from "$api/fetch";
+	import Button from '$lib5/button/Button.svelte';
+	import {
+		IS_DEV,
+		TPL_RESTRICTED_EMAIL_DOMAIN,
+		TPL_CLIENT_LOGO_UPDATED,
+		TPL_CLIENT_NAME,
+		TPL_CLIENT_URL
+	} from '$utils/constants';
+	import Input from '$lib5/form/Input.svelte';
+	import LangSelector from '$lib5/LangSelector.svelte';
+	import Main from '$lib5/Main.svelte';
+	import ContentCenter from '$lib5/ContentCenter.svelte';
+	import { useI18n } from '$state/i18n.svelte';
+	import Template from '$lib5/Template.svelte';
+	import { useParam } from '$state/param.svelte';
+	import ThemeSwitch from '$lib5/ThemeSwitch.svelte';
+	import Form from '$lib5/form/Form.svelte';
+	import { PATTERN_USER_NAME } from '$utils/patterns';
+	import type { NewUserRegistrationRequest } from '$api/types/register.ts';
+	import { fetchPost } from '$api/fetch';
+	import ClientLogo from '$lib5/ClientLogo.svelte';
+	import IconHome from '$icons/IconHome.svelte';
 
-    let t = useI18n();
+	let clientId = useParam('client_id').get();
+	let clientName = $state('');
+	// we can't use undefined to avoid a JSON error in the Template component
+	let clientLogoUpdated = $state(-1);
+	let clientUri = $state(IS_DEV ? '/auth/v1' : '');
 
-    let restrictedDomain = $state('');
-    let redirectUri = useParam('redirect_uri');
-    let isLoading = $state(false);
-    let err = $state('');
-    let success = $state(false);
+	let t = useI18n();
 
-    let action = $derived(IS_DEV ? '/auth/v1/dev/register' : '/auth/v1/users/register');
+	let restrictedDomain = $state('');
+	let redirectUri = useParam('redirect_uri');
+	let isLoading = $state(false);
+	let err = $state('');
+	let success = $state(false);
 
-    async function onSubmit(form: HTMLFormElement, params: URLSearchParams) {
-        success = false;
-        err = '';
+	let action = $derived(IS_DEV ? '/auth/v1/dev/register' : '/auth/v1/users/register');
 
-        let email = params.get('email');
-        let given_name = params.get('given_name');
-        let pow = params.get('pow');
+	async function onSubmit(form: HTMLFormElement, params: URLSearchParams) {
+		success = false;
+		err = '';
 
-        if (!email || !given_name || !pow) {
-            console.error('email, given_name, pow missing');
-            return;
-        }
+		let email = params.get('email');
+		let given_name = params.get('given_name');
+		let pow = params.get('pow');
 
-        if (restrictedDomain && !email.endsWith(restrictedDomain)) {
-            err = t.register.domainErr;
-            return;
-        }
+		if (!email || !given_name || !pow) {
+			console.error('email, given_name, pow missing');
+			return;
+		}
 
-        let payload: NewUserRegistrationRequest = {
-            email,
-            given_name,
-            family_name: params.get('family_name') || undefined,
-            pow,
-            redirect_uri: params.get('redirect_uri') || undefined,
-        };
+		if (restrictedDomain && !email.endsWith(restrictedDomain)) {
+			err = t.register.domainErr;
+			return;
+		}
 
-        isLoading = true;
-        // await tick();
+		let payload: NewUserRegistrationRequest = {
+			email,
+			given_name,
+			family_name: params.get('family_name') || undefined,
+			pow,
+			redirect_uri: params.get('redirect_uri') || undefined
+		};
 
-        const res = await fetchPost(form.action, payload);
-        if (res.error) {
-            let error = res.error.message || 'Error';
-            if (error.includes("UNIQUE constraint")) {
-                err = 'E-Mail is already registered';
-            } else {
-                err = error;
-            }
-        } else {
-            err = '';
-            success = true;
-            if (redirectUri) {
-                setTimeout(() => {
-                    window.location.replace(payload.redirect_uri || '/auth/v1/account');
-                }, 3000);
-            }
-        }
+		isLoading = true;
+		// await tick();
 
-        isLoading = false;
-    }
+		const res = await fetchPost(form.action, payload);
+		if (res.error) {
+			let error = res.error.message || 'Error';
+			if (error.includes('UNIQUE constraint')) {
+				err = 'E-Mail is already registered';
+			} else {
+				err = error;
+			}
+		} else {
+			err = '';
+			success = true;
+			if (redirectUri) {
+				setTimeout(() => {
+					window.location.replace(payload.redirect_uri || '/auth/v1/account');
+				}, 3000);
+			}
+		}
+
+		isLoading = false;
+	}
 </script>
 
 <svelte:head>
-    <title>{t?.register.register || 'Register'}</title>
+	<title>{t?.register.register || 'Register'}</title>
 </svelte:head>
 
-<Template id={TPL_RESTRICTED_EMAIL_DOMAIN} bind:value={restrictedDomain}/>
+<Template id={TPL_RESTRICTED_EMAIL_DOMAIN} bind:value={restrictedDomain} />
+<Template id={TPL_CLIENT_NAME} bind:value={clientName} />
+<Template id={TPL_CLIENT_URL} bind:value={clientUri} />
+<Template id={TPL_CLIENT_LOGO_UPDATED} bind:value={clientLogoUpdated} />
 
 <Main>
-    <ContentCenter>
-        <div class="container">
-            <div class="domainTxt">
-                <h1>{t.register.userReg}</h1>
-                {#if restrictedDomain}
-                    {t.register.domainRestricted}<br>
-                    {t.register.domainAllowed} <code>@{restrictedDomain}</code>
-                {/if}
-            </div>
+	<div class="outer">
+		<ContentCenter>
+			<div class="container">
+				<div class="head">
+					<ClientLogo
+						clientId={clientId || 'rauthy'}
+						updated={clientLogoUpdated > -1 ? clientLogoUpdated : undefined}
+					/>
+					{#if clientUri}
+						<a class="home" href={clientUri} aria-label="Client Home Page">
+							<IconHome color="hsla(var(--text) / .9)" />
+						</a>
+					{/if}
+				</div>
+				<div class="domainTxt">
+					<h1>{t.register.userReg}</h1>
+					{#if restrictedDomain}
+						{t.register.domainRestricted}<br />
+						{t.register.domainAllowed} <code>@{restrictedDomain}</code>
+					{/if}
+				</div>
 
-            <Form {action} {onSubmit} withPowAs="pow">
-                {#if redirectUri.get()}
-                    <input type="hidden" name="redirect_uri" value={redirectUri.get()}/>
-                {/if}
-                <Input
-                        typ="email"
-                        name="email"
-                        autocomplete="email"
-                        label={t.common.email}
-                        placeholder={t.common.email}
-                        required
-                />
-                <Input
-                        name="given_name"
-                        autocomplete="given-name"
-                        label={t.account.givenName}
-                        placeholder={t.account.givenName}
-                        pattern={PATTERN_USER_NAME}
-                        required
-                />
-                <Input
-                        name="family_name"
-                        autocomplete="family-name"
-                        label={t.account.familyName}
-                        placeholder={t.account.familyName}
-                        pattern={PATTERN_USER_NAME}
-                />
+				<Form {action} {onSubmit} withPowAs="pow">
+					{#if redirectUri.get()}
+						<input type="hidden" name="redirect_uri" value={redirectUri.get()} />
+					{/if}
+					<Input
+						typ="email"
+						name="email"
+						autocomplete="email"
+						label={t.common.email}
+						placeholder={t.common.email}
+						required
+					/>
+					<Input
+						name="given_name"
+						autocomplete="given-name"
+						label={t.account.givenName}
+						placeholder={t.account.givenName}
+						pattern={PATTERN_USER_NAME}
+						required
+					/>
+					<Input
+						name="family_name"
+						autocomplete="family-name"
+						label={t.account.familyName}
+						placeholder={t.account.familyName}
+						pattern={PATTERN_USER_NAME}
+					/>
 
-                <div class="submit">
-                    <Button type="submit" {isLoading}>{t.register.register}</Button>
-                </div>
-                {#if success}
-                    <div class="success">
-                        {t.register.success}<br/>
-                        {t.register.emailCheck}
-                    </div>
-                {:else if err}
-                    <div class="err">
-                        {err}
-                    </div>
-                {/if}
+					<div class="submit">
+						<Button type="submit" {isLoading}>{t.register.register}</Button>
+					</div>
+					{#if success}
+						<div class="success">
+							{t.register.success}<br />
+							{t.register.emailCheck}
+						</div>
+					{:else if err}
+						<div class="err">
+							{err}
+						</div>
+					{/if}
+				</Form>
+			</div>
 
-            </Form>
-        </div>
-
-        <ThemeSwitch absolute/>
-        <LangSelector absolute/>
-    </ContentCenter>
+			<ThemeSwitch absolute />
+			<LangSelector absolute />
+		</ContentCenter>
+	</div>
 </Main>
 
 <style>
-    .submit {
-        margin-top: .66rem;
-    }
+	.submit {
+		margin-top: 0.66rem;
+	}
 
-    .container {
-        padding: 1rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        border-radius: var(--border-radius);
-        background: hsla(var(--bg-high) / .25);
-    }
+	.outer {
+		width: 100dvw;
+		height: 100dvh;
+		background: hsla(var(--bg-high) / 0.25);
+	}
 
-    .err {
-        max-width: 16rem;
-    }
+	.container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		max-width: 21rem;
+		padding: 20px;
+		border-radius: 5px;
+		border: 1px solid hsl(var(--bg-high));
+		background: hsl(var(--bg));
+	}
 
-    .err, .success {
-        margin-top: .5rem;
-    }
+	.err {
+		max-width: 16rem;
+	}
 
-    .domainTxt {
-        margin: .5rem 0;
-    }
+	.err,
+	.success {
+		margin: 0.5rem 0;
+	}
+
+	.domainTxt {
+		margin: 0 0.5rem;
+	}
+
+	.head {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.home {
+		margin-right: 5px;
+		cursor: pointer;
+	}
 </style>
