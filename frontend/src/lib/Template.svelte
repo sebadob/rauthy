bb15f7d240b85833b46bf521c9f0b70810a7b713
sebadob <sebastiandobe@mailbox.org>
2025-11-27 10:59:27 +0100

    remove `Template` from `.prettierignore`

--- frontend/src/lib/Template.svelte
+++ frontend/src/lib/Template.svelte
@@ -1,46 +1,45 @@
 <script lang="ts">
-	import {onMount} from "svelte";
-	import {fetchGet} from "$api/fetch";
-	import {IS_DEV} from "$utils/constants";
+    import { onMount } from 'svelte';
+    import { fetchGet } from '$api/fetch';
+    import { IS_DEV } from '$utils/constants';
 
-	// Note: prettier will remove the whole content of this file for whatever reason.
-	// Not auto-formatting here.
+    type T = $$Generic;
 
-	type T = $$Generic;
+    let {
+        id,
+        value = $bindable(),
+    }: {
+        id: string;
+        value: boolean | string | number | T;
+    } = $props();
 
-	let {
-		id,
-		value = $bindable(),
-	}: {
-		id: string,
-		value: boolean | string | number | T,
-	} = $props();
+    onMount(async () => {
+        if (!IS_DEV) {
+            let tpl = document.getElementById(id);
+            if (tpl) {
+                assign(tpl.innerHTML);
+            }
+        } else {
+            let res = await fetchGet<typeof value>(`/auth/v1/template/${id}`);
+            if (res.error) {
+                console.error(res.error);
+            } else if (res.text) {
+                assign(res.text);
+            }
+        }
+    });
 
-	onMount(async () => {
-		if (!IS_DEV) {
-			let tpl = document.getElementById(id);
-			if (tpl) {
-				assign(tpl.innerHTML);
-			}
-		} else {
-			let res = await fetchGet<typeof value>(`/auth/v1/template/${id}`);
-			if (res.error) {
-				console.error(res.error);
-			} else if (res.text) {
-				assign(res.text);
-			}
-		}
-	});
-
-	function assign(s: string) {
-		if (typeof value === 'boolean') {
-			value = s === 'true';
-		} else if (typeof value === 'string') {
-			value = s;
-		} else if (typeof value === 'number') {
-			value = Number.parseInt(s);
-		} else {
-			value = JSON.parse(s);
-		}
-	}
+    function assign(s: string) {
+        if (typeof value === 'boolean') {
+            value = s === 'true';
+        } else if (typeof value === 'string') {
+            value = s;
+        } else if (typeof value === 'number') {
+            value = Number.parseInt(s);
+        } else {
+            value = JSON.parse(s);
+        }
+    }
 </script>
+
+<!-- This empty comment is used to prevent prettier from wiping the file -->
