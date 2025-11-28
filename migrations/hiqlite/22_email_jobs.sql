6571d91fb43081125c2f008b40e59079ad739e65
sebadob <sebastiandobe@mailbox.org>
2025-11-28 11:06:53 +0100

    DB migrations for `email_jobs`

---
+++ migrations/hiqlite/22_email_jobs.sql
@@ -0,0 +1,22 @@
+CREATE TABLE email_jobs
+(
+    id           INTEGER NOT NULL
+        CONSTRAINT email_jobs_pk
+            PRIMARY KEY,
+    scheduled    INTEGER,
+    status       INTEGER NOT NULL,
+    updated      INTEGER NOT NULL,
+    last_user_ts INTEGER,
+    filter       TEXT,
+    subject      TEXT    NOT NULL,
+    body         TEXT    NOT NULL
+) STRICT;
+
+CREATE INDEX email_jobs_scheduled_index
+    ON email_jobs (scheduled);
+
+CREATE INDEX email_jobs_finished_index
+    ON email_jobs (finished);
+
+CREATE INDEX email_jobs_updated_index
+    ON email_jobs (updated);
---
+++ migrations/postgres/V17__email_jobs.sql
@@ -0,0 +1,22 @@
+CREATE TABLE email_jobs
+(
+    id           BIGINT   NOT NULL
+        CONSTRAINT email_jobs_pk
+            PRIMARY KEY,
+    scheduled    BIGINT,
+    status       SMALLINT NOT NULL,
+    updated      BIGINT   NOT NULL,
+    last_user_ts BIGINT,
+    filter       VARCHAR,
+    subject      VARCHAR  NOT NULL,
+    body         VARCHAR  NOT NULL
+);
+
+CREATE INDEX email_jobs_scheduled_index
+    ON email_jobs (scheduled);
+
+CREATE INDEX email_jobs_finished_index
+    ON email_jobs (finished);
+
+CREATE INDEX email_jobs_updated_index
+    ON email_jobs (updated);
