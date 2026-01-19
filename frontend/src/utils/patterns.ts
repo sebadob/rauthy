965f070e03afd9c71ce3fda36ee470be821bbc83
Benjamin Fernandez <fernandez.benjamin@outlook.com>
2026-01-19 08:25:14 +0100

    fix: Allow @ character in redirect_uri pattern

--- frontend/src/utils/patterns.ts
+++ frontend/src/utils/patterns.ts
@@ -25,6 +25,6 @@ export const PATTERN_ORIGIN = '^(http|https)://[a-z0-9.:\\-]+$';
 export const PATTERN_PHONE = '^\\+[0-9]{0,32}$';
 export const PATTERN_SCOPE_SPACE = '^[a-zA-Z0-9\\-_\\/:\\s*]{0,512}$';
 export const PATTERN_STREET = '^[a-zA-Z0-9À-ÿ\\-.\\s]{0,48}$';
-export const PATTERN_URI = "^[a-zA-Z0-9,.:\\/_\\-&?=~#!$'\\(\\)*+%]*$";
+export const PATTERN_URI = "^[a-zA-Z0-9,.:\\/_\\-&?=~#!$'\\(\\)*+%@]*$";
 export const PATTERN_USER_NAME =
     "^[a-zA-Z0-9À-ɏ\\-'\\s\\u3041-\\u3096\\u30A0-\\u30FF\\u3400-\\u4DB5\\u4E00-\\u9FCB\\uF900-\\uFA6A\\u2E80-\\u2FD5\\uFF66-\\uFF9F\\uFFA1-\\uFFDC\\u31F0-\\u31FF]{1,32}$";
