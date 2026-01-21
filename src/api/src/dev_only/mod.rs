6b30ea63f2193f776a1054414aeba89af008bc7d
sebadob <sebastiandobe@mailbox.org>
2026-01-21 10:31:19 +0100

    clippy

--- src/api/src/dev_only/mod.rs
+++ src/api/src/dev_only/mod.rs
@@ -1,5 +1,4 @@
 use actix_web::{HttpResponse, get, post};
-use rauthy_data::entity::browser_id::BrowserId;
 use rauthy_error::ErrorResponse;
 
 #[cfg(debug_assertions)]
@@ -36,7 +35,7 @@ pub async fn get_template() -> Result<HttpResponse, ErrorResponse> {
 pub async fn post_dev_only_endpoints(
     typ: actix_web::web::Path<String>,
     req: actix_web::HttpRequest,
-    browser_id: BrowserId,
+    browser_id: rauthy_data::entity::browser_id::BrowserId,
     payload: actix_web::web::Payload,
 ) -> Result<HttpResponse, ErrorResponse> {
     dev_handler::post_dev_only_endpoints(typ, req, browser_id, payload).await
