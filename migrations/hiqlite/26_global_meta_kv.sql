c18b6707593d60179d4b74784baf7691c0b9e6fe
sebadob <sebastiandobe@mailbox.org>
2026-03-11 11:49:43 +0100

    DB migrations

---
+++ migrations/hiqlite/26_global_meta_kv.sql
@@ -0,0 +1,35 @@
+CREATE TABLE kv_ns
+(
+    ns TEXT NOT NULL
+        CONSTRAINT kv_ns_pk
+            PRIMARY KEY
+) STRICT;
+
+create table kv_values
+(
+    ns        TEXT    not null
+        constraint kv_values_kv_ns_ns_fk
+            references kv_ns
+            on update cascade on delete cascade,
+    key       TEXT    not null,
+    encrypted INTEGER not null,
+    value     BLOB    not null,
+    CONSTRAINT kv_values_pk
+        PRIMARY KEY (ns, key)
+) STRICT;
+
+CREATE TABLE kv_access
+(
+    id      TEXT              NOT NULL
+        CONSTRAINT kv_access_pk
+            PRIMARY KEY,
+    ns      TEXT              NOT NULL
+        CONSTRAINT kv_access_kv_ns_ns_fk
+            REFERENCES kv_ns
+            ON UPDATE CASCADE ON DELETE CASCADE,
+    enabled INTEGER DEFAULT 1 NOT NULL,
+    name    TEXT
+) STRICT;
+
+CREATE INDEX kv_access_ns_index
+    ON kv_access (ns);
---
+++ migrations/postgres/V21__global_meta_kv.sql
@@ -0,0 +1,35 @@
+CREATE TABLE kv_ns
+(
+    ns VARCHAR NOT NULL
+        CONSTRAINT kv_ns_pk
+            PRIMARY KEY
+);
+
+create table kv_values
+(
+    ns        VARCHAR not null
+        constraint kv_values_kv_ns_ns_fk
+            references kv_ns
+            on update cascade on delete cascade,
+    key       VARCHAR not null,
+    encrypted BOOLEAN not null,
+    value     BYTEA   not null,
+    CONSTRAINT kv_values_pk
+        PRIMARY KEY (ns, key)
+);
+
+CREATE TABLE kv_access
+(
+    id      VARCHAR              NOT NULL
+        CONSTRAINT kv_access_pk
+            PRIMARY KEY,
+    ns      VARCHAR              NOT NULL
+        CONSTRAINT kv_access_kv_ns_ns_fk
+            REFERENCES kv_ns
+            ON UPDATE CASCADE ON DELETE CASCADE,
+    enabled BOOLEAN DEFAULT true NOT NULL,
+    name    VARCHAR
+);
+
+CREATE INDEX kv_access_ns_index
+    ON kv_access (ns);
