fa336971f24e4cce526550558868f9eaf93ebdf4
mega123-art <agrawalparth326@gmail.com>
2026-04-05 17:29:00 +0530

    refactor: remove global trailing slash redirect and implement specific admin path redirection in static asset handler

--- src/bin/src/server.rs
+++ src/bin/src/server.rs
@@ -681,8 +681,7 @@ fn api_services() -> actix_web::Scope {
                 .service(generic::get_ready)
                 .service(swagger_ui::get_openapi_doc)
                 .service(swagger_ui::get_swagger_ui)
-                .service(html::get_static_assets)
-                .default_service(web::to(generic::redirect_subroutes)),
+                .service(html::get_static_assets),
         )
 }
 
