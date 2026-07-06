fa0f3e62e886a0b23cd9c986501d03097a3c7fb1
sebadob <sebastiandobe@mailbox.org>
2026-07-06 13:23:43 +0200

    reformat UI types after `npm install` of updated deps

--- frontend/src/api/types/user.ts
+++ frontend/src/api/types/user.ts
@@ -95,7 +95,12 @@ export interface UserAttrValuesUpdateRequest {
 }
 
 export type UserAccountTypeResponse =
-    'new' | 'password' | 'passkey' | 'federated' | 'federated_passkey' | 'federated_password';
+    | 'new'
+    | 'password'
+    | 'passkey'
+    | 'federated'
+    | 'federated_passkey'
+    | 'federated_password';
 
 export interface UserResponseSimple {
     id: string;
